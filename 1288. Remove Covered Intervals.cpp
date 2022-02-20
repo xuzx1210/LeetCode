7d28b362fb7efd3a1f4e95ec391383f3d24469cf
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-02-21 02:11:40 +0800

    Update 1288. Remove Covered Intervals.cpp

--- 1288. Remove Covered Intervals.cpp
+++ 1288. Remove Covered Intervals.cpp
@@ -1,9 +1,17 @@
 class Solution
 {
 public:
+    static bool compare(vector<int> &a, vector<int> &b)
+    {
+        if (a[0] < b[0])
+            return true;
+        if (a[0] > b[0])
+            return false;
+        return a[1] > b[1];
+    }
     int removeCoveredIntervals(vector<vector<int>> &intervals)
     {
-        sort(intervals.begin(), intervals.end());
+        sort(intervals.begin(), intervals.end(), compare);
         int left = intervals[0][0], right = intervals[0][1];
         int result = 1;
         for (int i = 1; i < intervals.size(); ++i)
