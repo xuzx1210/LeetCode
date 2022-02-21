c8177425681d8ca8cae3839f568fcb79183e6a6b
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-02-21 20:51:03 +0800

    Update 27. Remove Element.cpp

--- 27. Remove Element.cpp
+++ 27. Remove Element.cpp
@@ -3,20 +3,9 @@ class Solution
 public:
     int removeElement(vector<int> &nums, int val)
     {
-        for (int i = 0; i < nums.size(); ++i)
+        for (int i = nums.size() - 1; i >= 0; --i)
             if (nums[i] == val)
-            {
-                bool found = false;
-                for (int j = i + 1; j < nums.size(); ++j)
-                    if (nums[j] != val)
-                    {
-                        swap(nums[i], nums[j]);
-                        found = true;
-                        break;
-                    }
-                if (!found)
-                    return i;
-            }
+                nums.erase(nums.begin() + i);
         return nums.size();
     }
 };
