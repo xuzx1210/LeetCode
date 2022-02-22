b3561aea10b720af919051216da0e3fb1f09bf0b
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-02-22 17:24:20 +0800

    Update 171. Excel Sheet Column Number.cpp

--- 171. Excel Sheet Column Number.cpp
+++ 171. Excel Sheet Column Number.cpp
@@ -4,8 +4,8 @@ public:
     int titleToNumber(string columnTitle)
     {
         int result = 0;
-        for (int i = 0; i < columnTitle.length(); ++i)
-            result = result * 26 - 'A' + columnTitle[i] + 1;
+        for (auto c : columnTitle)
+            result = result * 26 + (c - 'A' + 1);
         return result;
     }
 };
