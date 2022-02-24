1101d5aca6182ae593052154f6acde55ee7fdafb
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-02-25 04:49:10 +0800

    Create 59. Spiral Matrix II.cpp

---
+++ 59. Spiral Matrix II.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    vector<vector<int>> generateMatrix(int n)
+    {
+        vector<vector<int>> result(n, vector<int>(n, 0));
+        int num = 1, half = n >> 1;
+        for (int i = 0; i < half; ++i)
+        {
+            int back = n - 1 - i;
+            int length = back - i;
+            for (int j = i; j < back; ++j)
+                result[i][j] = num++;
+            for (int j = i; j < back; ++j)
+                result[j][back] = num++;
+            for (int j = back; j > i; --j)
+                result[back][j] = num++;
+            for (int j = back; j > i; --j)
+                result[j][i] = num++;
+        }
+        if (n & 1)
+            result[half][half] = num;
+        return result;
+    }
+};
