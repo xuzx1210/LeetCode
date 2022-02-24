e751dd9fe841284e106bec55bc642d7452921b6b
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-02-24 08:02:55 +0800

    Create 46. Permutations.cpp

---
+++ 46. Permutations.cpp
@@ -0,0 +1,12 @@
+class Solution
+{
+public:
+    vector<vector<int>> permute(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result(1, nums);
+        while (next_permutation(nums.begin(), nums.end()))
+            result.push_back(nums);
+        return result;
+    }
+};
