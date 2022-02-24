93df079869d118e2bfc65b4605533f77e68ade94
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-02-25 06:43:58 +0800

    Update 63. Unique Paths II.cpp

--- 63. Unique Paths II.cpp
+++ 63. Unique Paths II.cpp
@@ -9,8 +9,8 @@ public:
             return 0;
         for (int i = 1; i < m; ++i)
             pathCount[i][0] = (pathCount[i - 1][0] && !obstacleGrid[i][0]);
-        for (int j = 1; j < n; ++j)
-            pathCount[0][j] = (pathCount[0][j - 1] && !obstacleGrid[0][j]);
+        for (int i = 1; i < n; ++i)
+            pathCount[0][i] = (pathCount[0][i - 1] && !obstacleGrid[0][i]);
         for (int i = 1; i < m; ++i)
             for (int j = 1; j < n; ++j)
                 pathCount[i][j] = obstacleGrid[i][j] ? 0 : (pathCount[i - 1][j] + pathCount[i][j - 1]);
