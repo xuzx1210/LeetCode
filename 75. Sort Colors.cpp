55af649d01d31a573da9210196b5bee917d5fe2d
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-02-28 05:31:09 +0800

    Update 75. Sort Colors.cpp
    no sort

--- 75. Sort Colors.cpp
+++ 75. Sort Colors.cpp
@@ -3,6 +3,11 @@ class Solution
 public:
     void sortColors(vector<int> &nums)
     {
-        sort(nums.begin(), nums.end());
+        int count[3];
+        for (auto num : nums)
+            ++count[num];
+        nums.clear();
+        for (int i = 0; i < 3; ++i)
+            nums.insert(nums.end(), count[i], i);
     }
 };
