bb48241a5b0053f411513dc44ad24a3a8a0788aa
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-03-01 10:16:55 +0800

    Update 338. Counting Bits.cpp
    right shift , and then plus one if odd

--- 338. Counting Bits.cpp
+++ 338. Counting Bits.cpp
@@ -5,7 +5,7 @@ public:
     {
         vector<int> result(n + 1, 0);
         for (int i = 1; i <= n; ++i)
-            result[i] = result[i & (i - 1)] + 1;
+            result[i] = result[i >> 1] + (i & 1);
         return result;
     }
 };
