7b3aa8206ce564b50063002babda6cab633f8d33
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-03-05 07:09:40 +0800

    Update 106. Construct Binary Tree from Inorder and Postorder Traversal.cpp

--- 106. Construct Binary Tree from Inorder and Postorder Traversal.cpp
+++ 106. Construct Binary Tree from Inorder and Postorder Traversal.cpp
@@ -8,8 +8,7 @@ public:
         int mid = postorder[postRight - 1];
         int inPos = find(inorder.begin() + inLeft, inorder.begin() + inRight, mid) - inorder.begin();
         int postPos = postLeft + inPos - inLeft;
-        TreeNode *result = new TreeNode(mid, makeTree(inorder, postorder, inLeft, inPos, postLeft, postPos), makeTree(inorder, postorder, inPos + 1, inRight, postPos, postRight - 1));
-        return result;
+        return new TreeNode(mid, makeTree(inorder, postorder, inLeft, inPos, postLeft, postPos), makeTree(inorder, postorder, inPos + 1, inRight, postPos, postRight - 1));
     }
     TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder)
     {
