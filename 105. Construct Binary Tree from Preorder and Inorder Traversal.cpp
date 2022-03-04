95736bb7ba7b8513062f880335052f3527cc18d8
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-03-05 06:56:31 +0800

    Update 105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
    no copy new vector

--- 105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ 105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -1,17 +1,18 @@
 class Solution
 {
 public:
+    TreeNode *makeTree(vector<int> &preorder, vector<int> &inorder, int preLeft, int preRight, int inLeft, int inRight)
+    {
+        if (preLeft == preRight)
+            return nullptr;
+        int mid = preorder[preLeft];
+        int inPos = find(inorder.begin() + inLeft, inorder.begin() + inRight, mid) - inorder.begin();
+        int prePos = preLeft + 1 + inPos - inLeft;
+        TreeNode *result = new TreeNode(mid, makeTree(preorder, inorder, preLeft + 1, prePos, inLeft, inPos), makeTree(preorder, inorder, prePos, preRight, inPos + 1, inRight));
+        return result;
+    }
     TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder)
     {
-        int mid = preorder[0];
-        auto it = find(inorder.begin(), inorder.end(), mid);
-        vector<int> leftInorder(inorder.begin(), it), rightInorder(it + 1, inorder.end());
-        vector<int> leftPreorder(preorder.begin() + 1, preorder.begin() + 1 + leftInorder.size()), rightPreorder(preorder.begin() + 1 + leftInorder.size(), preorder.end());
-        TreeNode *cur = new TreeNode(mid);
-        if (!leftInorder.empty())
-            cur->left = buildTree(leftPreorder, leftInorder);
-        if (!rightInorder.empty())
-            cur->right = buildTree(rightPreorder, rightInorder);
-        return cur;
+        return makeTree(preorder, inorder, 0, preorder.size(), 0, inorder.size());
     }
 };
