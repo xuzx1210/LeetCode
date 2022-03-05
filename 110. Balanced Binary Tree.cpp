42fbe223c1380e11d43315e1b351be8c6e647a81
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-03-05 08:00:12 +0800

    Update 110. Balanced Binary Tree.cpp
    use -1 to check status

--- 110. Balanced Binary Tree.cpp
+++ 110. Balanced Binary Tree.cpp
@@ -1,21 +1,18 @@
 class Solution
 {
 public:
-    int maxDepth(TreeNode *cur, bool &over1)
+    int maxDepth(TreeNode *cur)
     {
         if (!cur)
             return 0;
-        int leftDepth = maxDepth(cur->left, over1), rightDepth = maxDepth(cur->right, over1);
-        int diff = leftDepth - rightDepth;
-        diff = max(diff, -diff);
-        if (diff > 1)
-            over1 = true;
+        int leftDepth = maxDepth(cur->left), rightDepth = maxDepth(cur->right);
+        if (leftDepth == -1 || rightDepth == -1 || abs(leftDepth - rightDepth) > 1)
+            return -1;
         return max(leftDepth, rightDepth) + 1;
     }
     bool isBalanced(TreeNode *root)
     {
         bool over1 = false;
-        maxDepth(root, over1);
-        return !over1;
+        return maxDepth(root) != -1;
     }
 };
