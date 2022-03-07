8fd36d1ab8edd338fd07ff0d1a8c81133e163262
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-03-07 08:52:08 +0800

    Update 21. Merge Two Sorted Lists.cpp
    recursive

--- 21. Merge Two Sorted Lists.cpp
+++ 21. Merge Two Sorted Lists.cpp
@@ -3,24 +3,19 @@ class Solution
 public:
     ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
     {
-        ListNode *result = new ListNode;
-        ListNode *cur = result;
-        while (list1 != nullptr && list2 != nullptr)
+        if (!list1)
+            return list2;
+        if (!list2)
+            return list1;
+        if (list1->val < list2->val)
         {
-            if (list1->val < list2->val)
-            {
-                cur->next = list1;
-                cur = list1;
-                list1 = list1->next;
-            }
-            else
-            {
-                cur->next = list2;
-                cur = list2;
-                list2 = list2->next;
-            }
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
         }
-        cur->next = (list1 == nullptr) ? list2 : list1;
-        return result->next;
+        else
+        {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
     }
 };
