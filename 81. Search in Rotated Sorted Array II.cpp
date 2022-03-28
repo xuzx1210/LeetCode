755ad65744752137ce3c46778c79cf4bce06fda5
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-03-28 21:51:26 +0800

    Create 81. Search in Rotated Sorted Array II.cpp

---
+++ 81. Search in Rotated Sorted Array II.cpp
@@ -0,0 +1,11 @@
+class Solution
+{
+public:
+    bool search(vector<int> &nums, int target)
+    {
+        for (auto num : nums)
+            if (num == target)
+                return true;
+        return false;
+    }
+};
