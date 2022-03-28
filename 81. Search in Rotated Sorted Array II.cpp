0a029dbf2a13f6a67bdcbb06ea5fabe8158aff88
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-03-28 21:52:55 +0800

    Update 81. Search in Rotated Sorted Array II.cpp

--- 81. Search in Rotated Sorted Array II.cpp
+++ 81. Search in Rotated Sorted Array II.cpp
@@ -3,9 +3,6 @@ class Solution
 public:
     bool search(vector<int> &nums, int target)
     {
-        for (auto num : nums)
-            if (num == target)
-                return true;
-        return false;
+        return find(nums.begin(), nums.end(), target) != nums.end();
     }
 };
