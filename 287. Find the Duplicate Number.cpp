d1d6bf5a76e360f1aa19cf6f58ba853af661441d
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-03-29 19:34:18 +0800

    Update 287. Find the Duplicate Number.cpp

--- 287. Find the Duplicate Number.cpp
+++ 287. Find the Duplicate Number.cpp
@@ -6,9 +6,9 @@ public:
         vector<bool> exist(nums.size() - 1, false);
         for (auto num : nums)
         {
-            if (exist[num])
+            if (exist[num - 1])
                 return num;
-            exist[num] = true;
+            exist[num - 1] = true;
         }
         return 0;
     }
