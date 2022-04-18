0f073afbd64d9eed0e8af94b81c9f05aa3874565
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-04-18 22:32:28 +0800

    Create 682. Baseball Game.cpp

---
+++ 682. Baseball Game.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int calPoints(vector<string> &ops)
+    {
+        vector<int> scores({});
+        for (string op : ops)
+            if (op == "+")
+                scores.push_back(scores[scores.size() - 1] + scores[scores.size() - 2]);
+            else if (op == "D")
+                scores.push_back(scores.back() * 2);
+            else if (op == "C")
+                scores.pop_back();
+            else
+                scores.push_back(stoi(op));
+        int result = 0;
+        for (int score : scores)
+            result += score;
+        return result;
+    }
+};
