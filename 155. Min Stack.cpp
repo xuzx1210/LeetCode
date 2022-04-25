23ab0af1503133b7a0498bf63fd0725ab84f5fc2
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-04-26 03:38:28 +0800

    Create 155. Min Stack.cpp

---
+++ 155. Min Stack.cpp
@@ -0,0 +1,34 @@
+class MinStack
+{
+private:
+    stack<int> nums, mins;
+
+public:
+    MinStack()
+    {
+        while (!nums.empty())
+            nums.pop();
+        while (!mins.empty())
+            mins.pop();
+        mins.push(INT_MAX);
+    }
+    void push(int val)
+    {
+        nums.push(val);
+        int m = mins.top();
+        mins.push(m < val ? m : val);
+    }
+    void pop()
+    {
+        nums.pop();
+        mins.pop();
+    }
+    int top()
+    {
+        return nums.top();
+    }
+    int getMin()
+    {
+        return mins.top();
+    }
+};
