6571eec54e8b772706d9f3b376ae662a00dbf55c
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-05-13 02:17:51 +0800

    Update 47. Permutations II.cpp
    no next_permutation

--- 47. Permutations II.cpp
+++ 47. Permutations II.cpp
@@ -1,13 +1,38 @@
-
 class Solution
 {
-public:
-    vector<vector<int>> permute(vector<int> &nums)
+private:
+    unordered_map<int, int> counter;
+    size_t size;
+    vector<vector<int>> result;
+    vector<int> cur;
+    void dfs(size_t index)
     {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> result(1, nums);
-        while (next_permutation(nums.begin(), nums.end()))
-            result.push_back(nums);
+        if (index == size)
+        {
+            result.push_back(cur);
+            return;
+        }
+        for (auto &n : counter)
+            if (n.second)
+            {
+                --n.second;
+                cur.push_back(n.first);
+                dfs(index + 1);
+                cur.pop_back();
+                ++n.second;
+            }
+    }
+
+public:
+    vector<vector<int>> permuteUnique(vector<int> &nums)
+    {
+        size = nums.size();
+        result.clear();
+        cur.clear();
+        counter.clear();
+        for (int num : nums)
+            ++counter[num];
+        dfs(0);
         return result;
     }
 };
