7e9c0d67a4c1d65b38a8efcc23357aab1f4b3a6e
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-06-06 19:06:49 +0800

    Update 160. Intersection of Two Linked Lists.cpp

--- 160. Intersection of Two Linked Lists.cpp
+++ 160. Intersection of Two Linked Lists.cpp
@@ -1,12 +1,19 @@
-class Solution {
+class Solution
+{
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        set<ListNode *> nodes({});
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+    {
+        vector<ListNode *> a = {}, b = {};
         for (ListNode *cur = headA; cur; cur = cur->next)
-            nodes.insert(cur);
+            a.push_back(cur);
+        reverse(a.begin(), a.end());
         for (ListNode *cur = headB; cur; cur = cur->next)
-            if (nodes.find(cur) != nodes.end())
-                return cur;
-        return nullptr;
+            b.push_back(cur);
+        reverse(b.begin(), b.end());
+        size_t minSize = min(a.size(), b.size()), index = 0;
+        for (index = 0; index < minSize; ++index)
+            if (a[index] != b[index])
+                break;
+        return index ? a[index - 1] : nullptr;
     }
 };
