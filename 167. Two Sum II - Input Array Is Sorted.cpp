1568f39548dcd9b7b5a835670cfba766044314b3
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-06-10 03:30:25 +0800

    Update 167. Two Sum II - Input Array Is Sorted.cpp

--- 167. Two Sum II - Input Array Is Sorted.cpp
+++ 167. Two Sum II - Input Array Is Sorted.cpp
@@ -3,16 +3,16 @@ class Solution
 public:
     vector<int> twoSum(vector<int> &numbers, int target)
     {
-        int i = 0, j = numbers.size() - 1;
-        while (i < j)
+        int left = 0, right = numbers.size() - 1;
+        while (left < right)
         {
-            int sum = numbers[i] + numbers[j];
-            if (sum > target)
-                --j;
-            else if (sum < target)
-                ++i;
+            int sum = numbers[left] + numbers[right];
+            if (sum < target)
+                ++left;
+            else if (sum > target)
+                --right;
             else
-                return {i + 1, j + 1};
+                return {left + 1, right + 1};
         }
         return vector<int>();
     }
