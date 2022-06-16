9d0195c7c18e02e546981b32bb42be2f88458440
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-06-17 03:29:56 +0800

    Update 5. Longest Palindromic Substring.cpp

--- 5. Longest Palindromic Substring.cpp
+++ 5. Longest Palindromic Substring.cpp
@@ -1,29 +1,21 @@
 class Solution
 {
 public:
-    bool palindromic(string &s, int start, int end)
-    {
-        int length = end - start + 1;
-        for (int i = start; i < start + (length >> 1); ++i)
-            if (s[i] != s[2 * start + length - i - 1])
-                return false;
-        return true;
-    }
     string longestPalindrome(string s)
     {
-        int length = s.length();
-        string result;
-        result.clear();
-        for (int start = 0; start < length; ++start)
-        {
-            for (int end = start + result.length(); end < length; ++end)
-            {
-                if (end - start < result.length())
-                    continue;
-                if (palindromic(s, start, end))
-                    result = string(s, start, end - start + 1);
-            }
-        }
-        return result;
+        int n = s.length();
+        if (n == 1)
+            return s;
+        vector<vector<bool>> dp(n, vector<bool>(n + 1));
+        for (int i = 0; i < n; ++i)
+            dp[i][0] = dp[i][1] = true;
+        for (int length = 2; length <= n; ++length)
+            for (int start = 0; start <= n - length; ++start)
+                dp[start][length] = dp[start + 1][length - 2] && s[start] == s[start + length - 1];
+        for (int length = n; length >= 0; --length)
+            for (int start = 0; start <= n - length; ++start)
+                if (dp[start][length])
+                    return s.substr(start, length);
+        return s;
     }
 };
