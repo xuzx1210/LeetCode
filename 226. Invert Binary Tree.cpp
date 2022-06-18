c03d4afff657c66b032c7eaaf2842a73d857a9d1
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-06-18 21:08:58 +0800

    Create 226. Invert Binary Tree.cpp

---
+++ 226. Invert Binary Tree.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    TreeNode *invertTree(TreeNode *root)
+    {
+        if (!root)
+            return root;
+        invertTree(root->left);
+        invertTree(root->right);
+        swap(root->left, root->right);
+        return root;
+    }
+};
