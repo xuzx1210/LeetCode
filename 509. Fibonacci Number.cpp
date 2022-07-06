bcf1e7a193a1eceef0ba01db9b0115da741c28a6
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-07-06 21:20:13 +0800

    Update 509. Fibonacci Number.cpp
    no swap

--- 509. Fibonacci Number.cpp
+++ 509. Fibonacci Number.cpp
@@ -3,12 +3,14 @@ class Solution
 public:
     int fib(int n)
     {
-        int tmp = 1, result = 0;
-        for (int i = 1; i <= n; ++i)
-        {
-            tmp += result;
-            swap(tmp, result);
-        }
-        return result;
+        if (!n)
+            return 0;
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; ++i)
+            if (i & 1)
+                b += a;
+            else
+                a += b;
+        return n & 1 ? b : a;
     }
 };
