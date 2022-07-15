a8443a949136c477a4bf66b1c497ace30a1ec755
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-07-16 01:41:28 +0800

    Update 695. Max Area of Island.cpp
    early stop

--- 695. Max Area of Island.cpp
+++ 695. Max Area of Island.cpp
@@ -1,33 +1,42 @@
 class Solution
 {
+private:
+    int m, n;
+    void next(int x, int y, queue<pair<int, int>> &bfs, vector<vector<int>> &grid)
+    {
+        if (x < 0 || m <= x || y < 0 || n <= y)
+            return;
+        if (!grid[x][y])
+            return;
+        bfs.push({x, y});
+        grid[x][y] = 0;
+    }
+
 public:
     int maxAreaOfIsland(vector<vector<int>> &grid)
     {
-        int m = grid.size(), n = grid.front().size();
+        m = grid.size();
+        n = grid.front().size();
         int result = 0;
         for (int i = 0; i < m; ++i)
             for (int j = 0; j < n; ++j)
-            {
-                queue<pair<int, int>> bfs({{i, j}});
-                int area = 0;
-                while (!bfs.empty())
+                if (grid[i][j])
                 {
-                    pair<int, int> coordinate = bfs.front();
-                    bfs.pop();
-                    int x = coordinate.first, y = coordinate.second;
-                    if (x < 0 || m <= x || y < 0 || n <= y)
-                        continue;
-                    if (!grid[x][y])
-                        continue;
-                    ++area;
-                    grid[x][y] = 0;
-                    bfs.push({x - 1, y});
-                    bfs.push({x, y - 1});
-                    bfs.push({x, y + 1});
-                    bfs.push({x + 1, y});
+                    grid[i][j] = 0;
+                    queue<pair<int, int>> bfs({{i, j}});
+                    int area = 0;
+                    for (; !bfs.empty(); ++area)
+                    {
+                        pair<int, int> coordinate = bfs.front();
+                        bfs.pop();
+                        int x = coordinate.first, y = coordinate.second;
+                        next(x - 1, y, bfs, grid);
+                        next(x + 1, y, bfs, grid);
+                        next(x, y - 1, bfs, grid);
+                        next(x, y + 1, bfs, grid);
+                    }
+                    result = max(result, area);
                 }
-                result = max(result, area);
-            }
         return result;
     }
 };
