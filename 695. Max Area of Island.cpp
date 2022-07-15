05b38266428e5b0d6d8a022bc4e6eeafee70602a
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-07-16 01:18:28 +0800

    Update 695. Max Area of Island.cpp
    dfs -> bfs

--- 695. Max Area of Island.cpp
+++ 695. Max Area of Island.cpp
@@ -1,33 +1,33 @@
 class Solution
 {
-private:
-    int m, n;
-    vector<vector<int>> grid;
-    bool inGrid(int x, int y)
-    {
-        return 0 <= x && x < m && 0 <= y && y < n;
-    }
-    int dfs(int x, int y)
-    {
-        if (!inGrid(x, y))
-            return 0;
-        if (!grid[x][y])
-            return 0;
-        grid[x][y] = 0;
-        return dfs(x - 1, y) + dfs(x, y - 1) + dfs(x, y + 1) + dfs(x + 1, y) + 1;
-    }
-
 public:
     int maxAreaOfIsland(vector<vector<int>> &grid)
     {
-        m = grid.size();
-        n = grid.front().size();
-        this->grid = grid;
+        int m = grid.size(), n = grid.front().size();
         int result = 0;
         for (int i = 0; i < m; ++i)
             for (int j = 0; j < n; ++j)
-                if (this->grid[i][j])
-                    result = max(result, dfs(i, j));
+            {
+                queue<pair<int, int>> bfs({{i, j}});
+                int area = 0;
+                while (!bfs.empty())
+                {
+                    pair<int, int> coordinate = bfs.front();
+                    bfs.pop();
+                    int x = coordinate.first, y = coordinate.second;
+                    if (x < 0 || m <= x || y < 0 || n <= y)
+                        continue;
+                    if (!grid[x][y])
+                        continue;
+                    ++area;
+                    grid[x][y] = 0;
+                    bfs.push({x - 1, y});
+                    bfs.push({x, y - 1});
+                    bfs.push({x, y + 1});
+                    bfs.push({x + 1, y});
+                }
+                result = max(result, area);
+            }
         return result;
     }
 };
