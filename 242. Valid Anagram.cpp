381a2868ed63bc76dfa11c4461f094ed71882972
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-07-28 13:19:08 +0800

    Create 242. Valid Anagram.cpp

---
+++ 242. Valid Anagram.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    bool isAnagram(string s, string t)
+    {
+        map<char, int> letters = {};
+        for (char &c : s)
+            ++letters[c];
+        for (char &c : t)
+            --letters[c];
+        bool result = true;
+        for (auto &p : letters)
+            result = result && !p.second;
+        return result;
+    }
+};
