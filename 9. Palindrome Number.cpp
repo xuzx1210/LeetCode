45d9cc03592704a69de2b0dbe36c4c7a4f661d5a
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-08-03 23:44:43 +0800

    Update 9. Palindrome Number.cpp

--- 9. Palindrome Number.cpp
+++ 9. Palindrome Number.cpp
@@ -3,14 +3,9 @@ class Solution
 public:
     bool isPalindrome(int x)
     {
-        stringstream ss;
-        ss.clear();
-        ss << x;
-        string num;
-        ss >> num;
-        for (int i = 0; i < (num.length() >> 1); ++i)
-            if (num[i] != num[num.length() - i - 1])
-                return false;
-        return true;
+        string num = to_string(x);
+        string tmp = num;
+        reverse(tmp.begin(), tmp.end());
+        return num == tmp;
     }
 };
