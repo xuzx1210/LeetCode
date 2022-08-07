90f6849b718eaf6e055f533aa3524d525ef64817
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-08-07 20:07:15 +0800

    Update 1220. Count Vowels Permutation.cpp

--- 1220. Count Vowels Permutation.cpp
+++ 1220. Count Vowels Permutation.cpp
@@ -3,8 +3,7 @@ class Solution
 public:
     int countVowelPermutation(int n)
     {
-        int mod = 1000000007;
-        long long a = 1, e = 1, i = 1, o = 1, u = 1;
+        long long a = 1, e = 1, i = 1, o = 1, u = 1, mod = 1000000007;
         for (int j = 1; j < n; ++j)
         {
             long long tmpA = a, tmpE = e, tmpI = i, tmpO = o, tmpU = u;
