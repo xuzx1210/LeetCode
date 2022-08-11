a33b5d3f283324d126d85712d45742791bcd1803
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-08-11 18:26:44 +0800

    Update 98. Validate Binary Search Tree.cpp

--- 98. Validate Binary Search Tree.cpp
+++ 98. Validate Binary Search Tree.cpp
@@ -1,18 +1,20 @@
 class Solution
 {
-public:
-    bool dfs(TreeNode *cur, long long m, long long M)
+private:
+    bool dfs(TreeNode *cur, long long left, long long right)
     {
-        if (!(m < cur->val && cur->val < M))
+        if (!cur)
+            return true;
+        if (cur->val <= left || right <= cur->val)
             return false;
-        if (cur->left)
-            if (!dfs(cur->left, m, cur->val))
-                return false;
-        if (cur->right)
-            if (!dfs(cur->right, cur->val, M))
-                return false;
+        if (!dfs(cur->left, left, cur->val))
+            return false;
+        if (!dfs(cur->right, cur->val, right))
+            return false;
         return true;
     }
+
+public:
     bool isValidBST(TreeNode *root)
     {
         return dfs(root, LLONG_MIN, LLONG_MAX);
