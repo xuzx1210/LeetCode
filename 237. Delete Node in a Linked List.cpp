10df8983c6b1dfd275ce0070e33a90f3dd679246
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-08-14 04:06:06 +0800

    Create 237. Delete Node in a Linked List.cpp

---
+++ 237. Delete Node in a Linked List.cpp
@@ -0,0 +1,11 @@
+class Solution
+{
+public:
+    void deleteNode(ListNode *node)
+    {
+        ListNode *tmp = node->next;
+        node->val = tmp->val;
+        node->next = tmp->next;
+        delete tmp;
+    }
+};
