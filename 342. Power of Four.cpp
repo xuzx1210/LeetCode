d1770dadedac71d5a5bf5ad782bf7311fc7a3e6c
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-08-22 20:31:20 +0800

    Create 342. Power of Four.cpp

---
+++ 342. Power of Four.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    bool isPowerOfFour(int n)
+    {
+        if (n < 1)
+            return false;
+        while (n)
+        {
+            if (n == 1)
+                return true;
+            if (n & 3)
+                return false;
+            n >>= 2;
+        }
+        return false;
+    }
+};
