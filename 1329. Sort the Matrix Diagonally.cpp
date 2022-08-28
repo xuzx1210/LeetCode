999fc3bc4035449ebd978fc751861b0f550458e7
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-08-28 19:13:08 +0800

    Update 1329. Sort the Matrix Diagonally.cpp
    unordered_map => vector

--- 1329. Sort the Matrix Diagonally.cpp
+++ 1329. Sort the Matrix Diagonally.cpp
@@ -4,15 +4,15 @@ public:
     vector<vector<int>> diagonalSort(vector<vector<int>> &mat)
     {
         int m = mat.size(), n = mat.front().size();
-        unordered_map<int, priority_queue<int>> um = {};
+        vector<priority_queue<int>> diagonal(m + n - 1);
         for (int i = 0; i < m; ++i)
             for (int j = 0; j < n; ++j)
-                um[i - j].push(mat[i][j]);
+                diagonal[i - j + n - 1].push(mat[i][j]);
         for (int i = m - 1; i >= 0; --i)
             for (int j = n - 1; j >= 0; --j)
             {
-                mat[i][j] = um[i - j].top();
-                um[i - j].pop();
+                mat[i][j] = diagonal[i - j + n - 1].top();
+                diagonal[i - j + n - 1].pop();
             }
         return mat;
     }
