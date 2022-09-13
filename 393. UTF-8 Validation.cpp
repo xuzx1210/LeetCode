2ef6ab5c5c22f35dd2cce3eaf6ee777f6216f72d
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-09-13 11:17:19 +0800

    Update 393. UTF-8 Validation.cpp

--- 393. UTF-8 Validation.cpp
+++ 393. UTF-8 Validation.cpp
@@ -1,40 +1,25 @@
 class Solution
 {
-private:
-    bool check(vector<int> &data, int index, int times)
-    {
-        if (index + times > data.size())
-            return false;
-        for (int i = 0; i < times; ++i)
-            if ((data[index + i] & 192) != 128)
-                return false;
-        return true;
-    }
-
 public:
     bool validUtf8(vector<int> &data)
     {
-        int size = data.size();
-        for (int i = 0; i < size; ++i)
-            if (!(data[i] & 128))
-                continue;
-            else if ((data[i] & 224) == 192)
-                if (check(data, i + 1, 1))
-                    ++i;
+        int counter = 0;
+        for (int &code : data)
+            if (!counter)
+                if ((code & 0b10000000) == 0b00000000)
+                    continue;
+                else if ((code & 0b11100000) == 0b11000000)
+                    counter = 1;
+                else if ((code & 0b11110000) == 0b11100000)
+                    counter = 2;
+                else if ((code & 0b11111000) == 0b11110000)
+                    counter = 3;
                 else
                     return false;
-            else if ((data[i] & 240) == 224)
-                if (check(data, i + 1, 2))
-                    i += 2;
-                else
-                    return false;
-            else if ((data[i] & 248) == 240)
-                if (check(data, i + 1, 3))
-                    i += 3;
-                else
-                    return false;
-            else
+            else if ((code & 0b11000000) != 0b10000000)
                 return false;
-        return true;
+            else
+                --counter;
+        return !counter;
     }
 };
