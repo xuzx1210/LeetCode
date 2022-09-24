bda88e4f421a0efdbc4bfc4100198b720714aab6
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-09-24 17:10:13 +0800

    Update 113. Path Sum II.cpp

--- 113. Path Sum II.cpp
+++ 113. Path Sum II.cpp
@@ -1,33 +1,27 @@
 class Solution
 {
-public:
+private:
+    vector<int> path;
     vector<vector<int>> result;
-    vector<int> sumStack;
-    void dfs(TreeNode *cur, int remain)
+    void dfs(TreeNode *node, int remain)
     {
-        sumStack.push_back(cur->val);
-        remain -= cur->val;
-        if (!cur->left && !cur->right)
-        {
-            if (!remain)
-                result.push_back(sumStack);
-            sumStack.pop_back();
+        if (!node)
             return;
-        }
-        if (cur->left)
-            dfs(cur->left, remain);
-        if (cur->right)
-            dfs(cur->right, remain);
-        sumStack.pop_back();
+        remain -= node->val;
+        path.emplace_back(node->val);
+        if (!node->left && !node->right && !remain)
+            result.emplace_back(path);
+        dfs(node->left, remain);
+        dfs(node->right, remain);
+        path.pop_back();
     }
+
+public:
     vector<vector<int>> pathSum(TreeNode *root, int targetSum)
     {
+        path.clear();
         result.clear();
-        if (root)
-        {
-            sumStack.clear();
-            dfs(root, targetSum);
-        }
+        dfs(root, targetSum);
         return result;
     }
 };
