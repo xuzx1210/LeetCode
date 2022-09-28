1ffb966e6d8441f22e83ec69f83b9eb52bf81499
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-09-28 14:15:24 +0800

    Update 19. Remove Nth Node From End of List.cpp
    space complexity: O(N) => O(1)

--- 19. Remove Nth Node From End of List.cpp
+++ 19. Remove Nth Node From End of List.cpp
@@ -3,21 +3,15 @@ class Solution
 public:
     ListNode *removeNthFromEnd(ListNode *head, int n)
     {
-        vector<int> nums;
-        nums.clear();
-        for (ListNode *cur = head; cur != nullptr; cur = cur->next)
-            nums.push_back(cur->val);
-        if (nums.size() < 2)
-            return nullptr;
-        nums.erase(nums.end() - n);
-        ListNode *result = new ListNode(nums[0]);
-        ListNode *cur = result;
-        for (int i = 1; i < nums.size(); ++i)
-        {
-            ListNode *tmp = new ListNode(nums[i]);
-            cur->next = tmp;
-            cur = tmp;
-        }
-        return result;
+        ListNode *fast = head;
+        for (int i = 0; i < n; ++i)
+            fast = fast->next;
+        if (!fast)
+            return head->next;
+        ListNode *slow = head;
+        for (; fast->next; fast = fast->next)
+            slow = slow->next;
+        slow->next = n == 1 ? nullptr : slow->next->next;
+        return head;
     }
 };
