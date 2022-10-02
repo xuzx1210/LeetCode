0e3e002b4340a0fc2ac39998df8aab2bfc675d2e
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-10-02 17:23:45 +0800

    Update 1155. Number of Dice Rolls With Target Sum.cpp

--- 1155. Number of Dice Rolls With Target Sum.cpp
+++ 1155. Number of Dice Rolls With Target Sum.cpp
@@ -4,10 +4,10 @@ public:
     int numRollsToTarget(int n, int k, int target)
     {
         int mod = 1000000007;
-        vector<vector<unsigned int>> dp(n + 1, vector<unsigned int>(target + 1, 0));
+        vector<vector<int>> dp(n + 1, vector<int>(target + 1));
         dp[0][0] = 1;
         for (int dice = 1; dice <= n; ++dice)
-            for (int sum = 1; sum <= target; ++sum)
+            for (int sum = dice; sum <= target && sum <= dice * k; ++sum)
                 for (int face = 1; face <= k && face <= sum; ++face)
                     dp[dice][sum] = (dp[dice][sum] + dp[dice - 1][sum - face]) % mod;
         return dp[n][target];
