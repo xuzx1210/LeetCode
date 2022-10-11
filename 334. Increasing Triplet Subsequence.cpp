64436ce3448462ca81b2ce4130a4eb95cf634cec
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-10-11 10:25:43 +0800

    Create 334. Increasing Triplet Subsequence.cpp

---
+++ 334. Increasing Triplet Subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    bool increasingTriplet(vector<int> &nums)
+    {
+        int small = INT_MAX, big = INT_MAX;
+        for (int &num : nums)
+            if (num <= small)
+                small = num;
+            else if (num <= big)
+                big = num;
+            else
+                return true;
+        return false;
+    }
+};
