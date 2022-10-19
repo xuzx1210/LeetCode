dc54906fe4c2b2332479a0e08ef81eab57078b95
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-10-19 21:19:49 +0800

    Update 69. Sqrt(x).cpp
    binary search, not builtin

--- 69. Sqrt(x).cpp
+++ 69. Sqrt(x).cpp
@@ -3,6 +3,18 @@ class Solution
 public:
     int mySqrt(int x)
     {
-        return sqrt(x);
+        int left = 0, right = 46341;
+        while (left < right)
+        {
+            int mid = (left + right) >> 1;
+            int square = mid * mid;
+            if (square < x)
+                left = mid + 1;
+            else if (x < square)
+                right = mid;
+            else
+                return mid;
+        }
+        return left - 1;
     }
 };
