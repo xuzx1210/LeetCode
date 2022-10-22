e5cc0eb91766f673e08df45d82d0fb3696aa7d85
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-10-23 05:38:49 +0800

    Update 331. Verify Preorder Serialization of a Binary Tree.cpp

--- 331. Verify Preorder Serialization of a Binary Tree.cpp
+++ 331. Verify Preorder Serialization of a Binary Tree.cpp
@@ -3,23 +3,16 @@ class Solution
 public:
     bool isValidSerialization(string preorder)
     {
-        vector<bool> sharp;
-        int length = preorder.length();
-        int left = 0;
+        preorder.push_back(',');
+        int length = preorder.length(), left = 0, stack = 1;
         for (int right = 0; right < length; ++right)
             if (preorder[right] == ',')
             {
-                sharp.emplace_back(preorder[left] == '#');
+                if (!stack)
+                    return false;
+                stack += preorder[left] == '#' ? -1 : 1;
                 left = right + 1;
             }
-        sharp.emplace_back(preorder[left] == '#');
-        int stack = 1;
-        for (bool s : sharp)
-        {
-            if (!stack)
-                return false;
-            stack += s ? -1 : 1;
-        }
         return !stack;
     }
 };
