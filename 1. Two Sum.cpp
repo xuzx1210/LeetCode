6e2ee56e8617d2198eb04bb7830a01c1a659713f
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-10-27 17:08:32 +0800

    Update 1. Two Sum.cpp

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -3,24 +3,15 @@ class Solution
 public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
-        unordered_map<int, int> hash;
-        hash.clear();
+        unordered_map<int, int> index;
         int size = nums.size();
         for (int i = 0; i < size; ++i)
-            hash[nums[i]] = i;
-        for (int i = 0; i < size; ++i)
         {
-            unordered_map<int, int>::iterator it = hash.find(target - nums[i]);
-            if (it != hash.end())
-                if (it->second != i)
-                {
-                    vector<int> result;
-                    result.clear();
-                    result.push_back(i);
-                    result.push_back(it->second);
-                    return result;
-                }
+            int another = index[target - nums[i]];
+            if (another)
+                return {another - 1, i};
+            index[nums[i]] = i + 1;
         }
-        return vector<int>();
+        return {};
     }
 };
