5b3ca5e5c7280207fdd5041af33c1213f2c7d50f
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-10-28 16:24:22 +0800

    Update 49. Group Anagrams.cpp

--- 49. Group Anagrams.cpp
+++ 49. Group Anagrams.cpp
@@ -3,17 +3,16 @@ class Solution
 public:
     vector<vector<string>> groupAnagrams(vector<string> &strs)
     {
-        unordered_map<string, vector<string>> um;
-        um.clear();
-        for (auto str : strs)
+        unordered_map<string, vector<string>> group;
+        for (string &str : strs)
         {
             string tmp(str);
             sort(tmp.begin(), tmp.end());
-            um[tmp].push_back(str);
+            group[tmp].emplace_back(str);
         }
-        vector<vector<string>> result({});
-        for (auto it : um)
-            result.push_back(it.second);
+        vector<vector<string>> result;
+        for (auto &[key, value] : group)
+            result.emplace_back(value);
         return result;
     }
 };
