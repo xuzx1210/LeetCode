6cbe4b278a9d324cfd02751d4345a9ae67c6f7e1
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-10-29 16:31:47 +0800

    Update 2136. Earliest Possible Day of Full Bloom.cpp

--- 2136. Earliest Possible Day of Full Bloom.cpp
+++ 2136. Earliest Possible Day of Full Bloom.cpp
@@ -1,11 +1,15 @@
-class Solution {
+class Solution
+{
 public:
-    int earliestFullBloom(vector<int>& plantTime, vector<int>& growTime) {
+    int earliestFullBloom(vector<int> &plantTime, vector<int> &growTime)
+    {
         vector<int> indices(plantTime.size());
         iota(indices.begin(), indices.end(), 0);
-        sort(indices.begin(), indices.end(), [&](int i, int j) { return growTime[i] > growTime[j]; });
+        sort(indices.begin(), indices.end(), [&](int i, int j)
+             { return growTime[i] > growTime[j]; });
         int result = 0, startTime = 0;
-        for (int index : indices) {
+        for (int index : indices)
+        {
             startTime += plantTime[index];
             result = max(result, startTime + growTime[index]);
         }
