3067ed688d73cfeb240eda7183aba1a2ef5d3f9a
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-10-31 14:48:23 +0800

    Update 322. Coin Change.cpp

--- 322. Coin Change.cpp
+++ 322. Coin Change.cpp
@@ -3,18 +3,12 @@ class Solution
 public:
     int coinChange(vector<int> &coins, int amount)
     {
-        sort(coins.begin(), coins.end());
-        vector<int> dp(amount + 1, INT_MAX);
-        dp.front() = 0;
+        vector<int> coinUsed(amount + 1, amount + 1);
+        coinUsed[0] = 0;
         for (int i = 1; i <= amount; ++i)
-            for (int coin : coins)
-            {
-                if (i < coin)
-                    break;
-                if (dp[i - coin] == INT_MAX)
-                    continue;
-                dp[i] = min(dp[i], dp[i - coin] + 1);
-            }
-        return dp.back() == INT_MAX ? -1 : dp.back();
+            for (int &coin : coins)
+                if (coin <= i)
+                    coinUsed[i] = min(coinUsed[i], coinUsed[i - coin] + 1);
+        return coinUsed[amount] > amount ? -1 : coinUsed[amount];
     }
 };
