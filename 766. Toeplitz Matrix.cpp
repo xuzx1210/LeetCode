3698726a10cd74a587a455eb4583c17ae9587796
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-10-31 13:31:42 +0800

    Update 766. Toeplitz Matrix.cpp

--- 766. Toeplitz Matrix.cpp
+++ 766. Toeplitz Matrix.cpp
@@ -4,20 +4,10 @@ public:
     bool isToeplitzMatrix(vector<vector<int>> &matrix)
     {
         int m = matrix.size(), n = matrix[0].size();
-        for (int i = 0; i < m - 1; ++i)
-        {
-            int first = matrix[i][0], limit = min(m - i, n);
-            for (int j = 1; j < limit; ++j)
-                if (matrix[i + j][j] != first)
+        for (int i = 1; i < m; ++i)
+            for (int j = 1; j < n; ++j)
+                if (matrix[i][j] != matrix[i - 1][j - 1])
                     return false;
-        }
-        for (int j = 1; j < n - 1; ++j)
-        {
-            int first = matrix[0][j], limit = min(m, n - j);
-            for (int i = 1; i < limit; ++i)
-                if (matrix[i][j + i] != first)
-                    return false;
-        }
         return true;
     }
 };
