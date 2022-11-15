8c98393edf4523c21ffd37a6c0052b65be2b7b20
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-11-15 16:14:23 +0800

    Update 222. Count Complete Tree Nodes.cpp

--- 222. Count Complete Tree Nodes.cpp
+++ 222. Count Complete Tree Nodes.cpp
@@ -3,12 +3,9 @@ class Solution
 public:
     int countNodes(TreeNode *root)
     {
-        if (!root)
-            return 0;
-        int leftDepth = 0;
+        int leftDepth = 0, rightDepth = 0;
         for (TreeNode *cur = root; cur; cur = cur->left)
             ++leftDepth;
-        int rightDepth = 0;
         for (TreeNode *cur = root; cur; cur = cur->right)
             ++rightDepth;
         return leftDepth == rightDepth ? (1 << leftDepth) - 1 : 1 + countNodes(root->left) + countNodes(root->right);
