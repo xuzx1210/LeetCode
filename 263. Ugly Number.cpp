44704b623f56db42b20061165690ac753254c2e1
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-11-19 00:28:37 +0800

    Update 263. Ugly Number.cpp

--- 263. Ugly Number.cpp
+++ 263. Ugly Number.cpp
@@ -3,14 +3,12 @@ class Solution
 public:
     bool isUgly(int n)
     {
-        if (!n)
+        if (n < 1)
             return false;
-        while ((n & 1) == 0)
-            n >>= 1;
-        while (n % 3 == 0)
-            n /= 3;
-        while (n % 5 == 0)
-            n /= 5;
+        vector<int> primes{2, 3, 5};
+        for (int &prime : primes)
+            while (!(n % prime))
+                n /= prime;
         return n == 1;
     }
 };
