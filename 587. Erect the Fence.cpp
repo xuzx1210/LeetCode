7ed86d99df078b34a6fb2b7a677802a2fafaa5c4
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-11-19 18:19:59 +0800

    Update 587. Erect the Fence.cpp
    using Andrew's monotone chain method
    upper(left to right) -> lower(right to left)

--- 587. Erect the Fence.cpp
+++ 587. Erect the Fence.cpp
@@ -2,25 +2,23 @@ class Solution
 {
 private:
     bool turnLeft(vector<int> &A, vector<int> &B, vector<int> &C)
-    {
+    { // cross product
         return (B[0] - A[0]) * (C[1] - B[1]) > (B[1] - A[1]) * (C[0] - B[0]);
     }
-    void convexHull(int i, vector<vector<int>> &result, vector<vector<int>> &trees)
-    {
-        for (int size = result.size(); size > 1; size = result.size())
-            if (turnLeft(result[size - 2], result.back(), trees[i]))
-                result.pop_back();
+    void convexHull(int i, vector<vector<int>> &stack, vector<vector<int>> &points)
+    { // turn right
+        for (int size = stack.size(); size > 1; size = stack.size())
+            if (turnLeft(stack[size - 2], stack.back(), points[i]))
+                stack.pop_back();
             else
                 break;
-        result.emplace_back(trees[i]);
+        stack.emplace_back(points[i]);
     }
 
 public:
     vector<vector<int>> outerTrees(vector<vector<int>> &trees)
-    {
+    { // Andrew's monotone chain
         const int size = trees.size();
-        if (size < 4)
-            return trees;
         auto compare = [](vector<int> &a, vector<int> &b)
         {
             if (a[0] < b[0])
@@ -31,11 +29,11 @@ public:
         };
         sort(trees.begin(), trees.end(), compare);
         vector<vector<int>> result;
-        for (int i = 0; i < size; ++i)
+        for (int i = 0; i < size; ++i) // upper
             convexHull(i, result, trees);
-        if (result.size() == size)
+        if (result.size() == size) // if all points form a line
             return result;
-        for (int i = size - 2; i >= 0; --i)
+        for (int i = size - 2; i >= 0; --i) // lower
             convexHull(i, result, trees);
         result.pop_back();
         return result;
