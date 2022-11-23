a345cc48a19f951fba322cbe0b42fd8b0f2e8698
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-11-24 03:33:56 +0800

    Update 394. Decode String.cpp

--- 394. Decode String.cpp
+++ 394. Decode String.cpp
@@ -10,9 +10,8 @@ private:
                 int k = 0; // count k
                 while (isdigit(s[index]))
                     k = k * 10 + (s[index++] - '0');
-                ++index; // skip '['
-                string encoded_string(dfs(s, index));
-                for (int i = 0; i < k; ++i) // add encoded_string k times
+                string encoded_string(dfs(s, ++index)); // skip '[' and find encoded_string
+                for (int i = 0; i < k; ++i)             // add encoded_string k times
                     result += encoded_string;
             }
             else if (islower(s[index])) // find a char
