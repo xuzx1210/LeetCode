b83d0dec640a84a20420a1686fa70250642bff30
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-12-07 03:53:24 +0800

    Update 423. Reconstruct Original Digits from English.cpp

--- 423. Reconstruct Original Digits from English.cpp
+++ 423. Reconstruct Original Digits from English.cpp
@@ -1,49 +1,20 @@
 class Solution
 {
-private:
-    class Table
-    {
-    private:
-        vector<int> counter;
-        char feature;
-
-    public:
-        Table(string num, char feature)
-        {
-            this->feature = feature;
-            counter.resize(26, 0);
-            for (char c : num)
-                ++counter[c - 'a'];
-        }
-        int operator-=(Table &right)
-        {
-            int times = counter[right.feature - 'a'];
-            for (int i = 0; i < 26; ++i)
-                counter[i] -= times * right.counter[i];
-            return times;
-        }
-    };
-
 public:
     string originalDigits(string s)
     {
-        Table input(s, 'a');
-        vector<Table> table{};
-        table.emplace_back("zero", 'z');
-        table.emplace_back("one", 'o');
-        table.emplace_back("two", 'w');
-        table.emplace_back("three", 't');
-        table.emplace_back("four", 'u');
-        table.emplace_back("five", 'f');
-        table.emplace_back("six", 'x');
-        table.emplace_back("seven", 's');
-        table.emplace_back("eight", 'g');
-        table.emplace_back("nine", 'i');
-        vector<int> resultCounter(10, 0);
-        for (int i = 0; i < 10; i += 2)
-            resultCounter[i] = input -= table[i];
-        for (int i = 1; i < 10; i += 2)
-            resultCounter[i] = input -= table[i];
+        const vector<string> digits{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        const vector<char> distinct{'z', 'o', 'w', 't', 'u', 'f', 'x', 's', 'g', 'i'};
+        const vector<int> order{0, 2, 4, 6, 8, 1, 3, 5, 7, 9};
+        vector<int> counter(26, 0), resultCounter(10, 0);
+        for (char c : s)
+            ++counter[c - 'a'];
+        for (int cur : order)
+        {
+            resultCounter[cur] = counter[distinct[cur] - 'a'];
+            for (char c : digits[cur])
+                counter[c - 'a'] -= resultCounter[cur];
+        }
         string result{};
         for (int i = 0; i < 10; ++i)
             result.insert(result.end(), resultCounter[i], i + '0');
