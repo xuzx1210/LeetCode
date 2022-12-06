b0cebb155c9564db32a41824fb7a624cb950f9d2
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-12-06 14:59:41 +0800

    Update 328. Odd Even Linked List.cpp

--- 328. Odd Even Linked List.cpp
+++ 328. Odd Even Linked List.cpp
@@ -3,19 +3,19 @@ class Solution
 public:
     ListNode *oddEvenList(ListNode *head)
     {
-        vector<ListNode *> nodes{};
-        for (ListNode *cur = head; cur; cur = cur->next)
-            nodes.emplace_back(cur);
-        size_t size = nodes.size();
-        for (size_t i = 0; i + 2 < size; i += 2)
-            nodes[i]->next = nodes[i + 2];
-        for (size_t i = 1; i + 2 < size; i += 2)
-            nodes[i]->next = nodes[i + 2];
-        if (size > 1)
+        if (!head)
+            return head;
+        if (!head->next)
+            return head;
+        ListNode *oddHead = head, *evenHead = head->next, *oddTail = head, *evenTail = head->next;
+        while (oddTail->next && evenTail->next)
         {
-            ((size & 1) ? nodes[size - 1] : nodes[size - 2])->next = nodes[1];
-            ((size & 1) ? nodes[size - 2] : nodes[size - 1])->next = nullptr;
+            oddTail->next = oddTail->next->next;
+            oddTail = oddTail->next;
+            evenTail->next = evenTail->next->next;
+            evenTail = evenTail->next;
         }
-        return head;
+        oddTail->next = evenHead;
+        return oddHead;
     }
 };
