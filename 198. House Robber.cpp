f12c8bd995b259084707bceabc90ad5a2e5ecd86
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-12-14 12:10:20 +0800

    Update 198. House Robber.cpp

--- 198. House Robber.cpp
+++ 198. House Robber.cpp
@@ -3,14 +3,18 @@ class Solution
 public:
     int rob(vector<int> &nums)
     {
-        int size = nums.size();
+        const size_t size = nums.size();
         if (size == 1)
             return nums[0];
-        vector<int> dp(size);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-        for (int i = 2; i < size; ++i)
-            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]);
-        return dp.back();
+        if (size == 2)
+            return max(nums[0], nums[1]);
+        int prev2 = nums[0], prev1 = max(nums[0], nums[1]), result;
+        for (size_t i = 2; i < size; ++i)
+        {
+            result = max(prev2 + nums[i], prev1);
+            prev2 = prev1;
+            prev1 = result;
+        }
+        return result;
     }
 };
