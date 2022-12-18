9c952703e88a5970a0d88bdc6bf0601366fb2d23
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-12-18 22:20:21 +0800

    Update 450. Delete Node in a BST.cpp

--- 450. Delete Node in a BST.cpp
+++ 450. Delete Node in a BST.cpp
@@ -4,7 +4,7 @@ public:
     TreeNode *deleteNode(TreeNode *root, int key)
     {
         if (!root)
-            return root;
+            return nullptr;
         if (key < root->val) // go left
         {
             root->left = deleteNode(root->left, key);
@@ -20,12 +20,19 @@ public:
             return nullptr;
         if (!root->left || !root->right) // root has one child
             return root->left ? root->left : root->right;
-        // root has two children
-        TreeNode *cur = root->left;
-        while (cur->right)
-            cur = cur->right;
-        root->val = cur->val;                          // root->val = successor
-        root->left = deleteNode(root->left, cur->val); // delete successor
+        // root has two children, delete successor or predecessor node
+        TreeNode *successor = root->left; // successor
+        while (successor->right)
+            successor = successor->right;
+        root->val = successor->val;                          // root->val = successor
+        root->left = deleteNode(root->left, successor->val); // delete successor node
+        /*
+        TreeNode *predecessor = root->right; // predecessor
+        while (predecessor->left)
+            predecessor = predecessor->left;
+        root->val = predecessor->val;                            // root->val = predecessor
+        root->right = deleteNode(root->right, predecessor->val); // delete predecessor node
+        */
         return root;
     }
 };
