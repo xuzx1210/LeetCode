58ca665fb1313e2fdf6d12fbd3c05fd2c97fdead
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2022-12-31 00:34:30 +0800

    Update 1834. Single-Threaded CPU.cpp

--- 1834. Single-Threaded CPU.cpp
+++ 1834. Single-Threaded CPU.cpp
@@ -1,51 +1,22 @@
 class Solution
 {
-private:
-    class Task
-    {
-    public:
-        size_t enqueueTime, processingTime, index;
-        void set(size_t s, size_t t, size_t i)
-        {
-            enqueueTime = s;
-            processingTime = t;
-            index = i;
-        }
-        bool operator<(const Task &right) const // two const are necessary, for priority_queue
-        {
-            if (processingTime > right.processingTime)
-                return true;
-            if (processingTime < right.processingTime)
-                return false;
-            return index > right.index;
-        }
-    };
-
 public:
     vector<int> getOrder(vector<vector<int>> &tasks)
     {
         const size_t size = tasks.size();
-        vector<Task> sortedByEnqueueTime(size);
         for (size_t i = 0; i < size; ++i)
-            sortedByEnqueueTime[i].set(tasks[i][0], tasks[i][1], i);
-        auto compareByEnqueueTime = [](const Task &a, const Task &b)
-        {
-            return a.enqueueTime < b.enqueueTime;
-        };
-        sort(sortedByEnqueueTime.begin(), sortedByEnqueueTime.end(), compareByEnqueueTime); // sort tasks by enqueueTime
-        priority_queue<Task> pq{};
+            tasks[i].emplace_back(i);     // enqueueTime, processTime, index
+        sort(tasks.begin(), tasks.end()); // sort by enqueueTime
         vector<int> result(size);
-        for (size_t timer = sortedByEnqueueTime[0].enqueueTime, enqueueIndex = 0, processIndex = 0; processIndex < size;)
+        priority_queue<pair<size_t, size_t>, vector<pair<size_t, size_t>>, greater<>> pq{}; // sort by 1. processIndex, 2. index
+        for (size_t timer = 0, enqueueIndex = 0, processIndex = 0; processIndex < size; ++processIndex)
         {
-            while (enqueueIndex < size && sortedByEnqueueTime[enqueueIndex].enqueueTime <= timer) // if enqueueTime <= timer
-                pq.emplace(sortedByEnqueueTime[enqueueIndex++]);
-            if (pq.empty()) // if cpu is idle
-            {
-                timer = sortedByEnqueueTime[enqueueIndex].enqueueTime; // get next enqueueTime
-                continue;
-            }
-            timer += pq.top().processingTime; // update timer
-            result[processIndex++] = pq.top().index;
+            if (pq.empty()) // if pq is empty, get next enqueueTime
+                timer = max(timer, (size_t)tasks[enqueueIndex][0]);
+            for (; enqueueIndex < size && tasks[enqueueIndex][0] <= timer; ++enqueueIndex)
+                pq.emplace(tasks[enqueueIndex][1], tasks[enqueueIndex][2]); // push processTime and index while enqueueTime <= timer
+            timer += pq.top().first;                                        // update timer
+            result[processIndex] = pq.top().second;
             pq.pop();
         }
         return result;
