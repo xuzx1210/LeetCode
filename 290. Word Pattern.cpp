8b4af90d1f2c2b8ee9dbe6e296fde984ea4aaf80
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-01-01 23:16:44 +0800

    Update 290. Word Pattern.cpp

--- 290. Word Pattern.cpp
+++ 290. Word Pattern.cpp
@@ -3,37 +3,30 @@ class Solution
 public:
     bool wordPattern(string pattern, string s)
     {
-        vector<string> words = {};
-        int start = 0;
-        const int sLength = s.length(), length = pattern.length();
-        for (int i = 0; i < sLength; ++i)
-            if (s[i] == ' ')
-            {
-                words.emplace_back(s.substr(start, i - start));
-                start = i + 1;
-            }
-        words.emplace_back(s.substr(start, sLength - start));
-        if (length != words.size())
+        vector<string> words{};
+        stringstream ss(s);
+        string word;
+        while (ss >> word)
+            words.emplace_back(word);
+        const size_t length = pattern.length(), size = words.size();
+        if (length != size)
             return false;
-        unordered_map<char, string> pattern2words = {};
-        unordered_map<string, char> words2pattern = {};
-        for (int i = 0; i < length; ++i)
+        unordered_map<char, string> p2w{};
+        unordered_map<string, char> w2p{};
+        for (size_t i = 0; i < size; ++i)
         {
-            auto p2w = pattern2words.find(pattern[i]);
-            auto w2p = words2pattern.find(words[i]);
-            if (p2w == pattern2words.end())
-            {
-                if (w2p == words2pattern.end())
-                {
-                    pattern2words[pattern[i]] = words[i];
-                    words2pattern[words[i]] = pattern[i];
-                }
-                else
-                    return false;
-            }
-            else if (w2p == words2pattern.end())
+            auto itp2w = p2w.find(pattern[i]);
+            auto itw2p = w2p.find(words[i]);
+            bool nop = itp2w == p2w.end(), now = itw2p == w2p.end();
+            if (nop && !now || !nop && now) // one exist
                 return false;
-            else if (pattern2words[pattern[i]] != words[i] || words2pattern[words[i]] != pattern[i])
+            if (nop) // no exist
+            {
+                p2w[pattern[i]] = words[i];
+                w2p[words[i]] = pattern[i];
+                continue;
+            }                                                               // both exist
+            if (p2w[pattern[i]] != words[i] || w2p[words[i]] != pattern[i]) // conflict
                 return false;
         }
         return true;
