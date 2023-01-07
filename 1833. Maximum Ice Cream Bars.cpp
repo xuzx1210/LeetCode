80a43e1ee701820618e225be61b3a3f782d036c6
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-01-07 18:05:35 +0800

    Update 1833. Maximum Ice Cream Bars.cpp

--- 1833. Maximum Ice Cream Bars.cpp
+++ 1833. Maximum Ice Cream Bars.cpp
@@ -6,8 +6,11 @@ public:
         sort(costs.begin(), costs.end());
         const size_t n = costs.size();
         for (size_t i = 0; i < n; ++i)
-            if ((coins -= costs[i]) < 0)
+        {
+            coins -= costs[i];
+            if (coins < 0)
                 return i;
+        }
         return n;
     }
 };
