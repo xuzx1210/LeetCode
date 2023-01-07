5fc1d729404f1aa73f1ad27c84e0ad942376e4df
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-01-08 05:36:00 +0800

    Update 134. Gas Station.cpp

--- 134. Gas Station.cpp
+++ 134. Gas Station.cpp
@@ -4,10 +4,11 @@ public:
     int canCompleteCircuit(vector<int> &gas, vector<int> &cost)
     {
         const size_t size = gas.size();
-        int minimum = INT_MAX, result = -1, tank = 0;
+        int minimum = INT_MAX, tank = 0, result;
         for (size_t i = 0; i < size; ++i)
         {
-            tank += (gas[i] - cost[i]);
+            tank += gas[i];
+            tank -= cost[i];
             if (tank < minimum)
             {
                 minimum = tank;
