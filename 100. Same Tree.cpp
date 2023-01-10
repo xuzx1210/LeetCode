dc084bfc65c6e5eae6abc48cc855300efd5bbb8b
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-01-10 08:20:44 +0800

    Update 100. Same Tree.cpp

--- 100. Same Tree.cpp
+++ 100. Same Tree.cpp
@@ -1,45 +1,14 @@
 class Solution
 {
 public:
-    bool dfs(TreeNode *p, TreeNode *q)
-    {
-        if (p->val != q->val)
-            return false;
-        if (p->left)
-            if (q->left)
-            {
-                if (!dfs(p->left, q->left))
-                    return false;
-            }
-            else
-                return false;
-        else if (q->left)
-            return false;
-        if (p->right)
-            if (q->right)
-            {
-                if (!dfs(p->right, q->right))
-                    return false;
-            }
-            else
-                return false;
-        else if (q->right)
-            return false;
-        return true;
-    }
     bool isSameTree(TreeNode *p, TreeNode *q)
     {
-        if (p)
-            if (q)
-                return dfs(p, q);
-            else
-                return false;
-        else
-        {
-            if (q)
-                return false;
-            else
-                return true;
-        }
+        if (!p && !q)
+            return true;
+        if (!p || !q)
+            return false;
+        if (p->val != q->val)
+            return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
