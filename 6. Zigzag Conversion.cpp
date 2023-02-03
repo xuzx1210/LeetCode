2a39bcec141479fc44e42f8ce0f4192ff096cea2
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-02-04 05:56:48 +0800

    Update 6. Zigzag Conversion.cpp

--- 6. Zigzag Conversion.cpp
+++ 6. Zigzag Conversion.cpp
@@ -1,45 +1,31 @@
 class Solution
 {
+private:
+    string result;
+    void addChar(const int index, string &s)
+    {
+        if (index < s.length())
+            result.push_back(s[index]);
+    }
+
 public:
     string convert(string s, int numRows)
     {
         if (numRows == 1)
             return s;
-        int length = s.length();
-        int halfGroupLength = numRows - 1;
-        int groupLength = halfGroupLength << 1;
-        vector<string> group;
-        group.clear();
-        for (int i = 0; i < length / groupLength; ++i)
-            group.push_back(string(s, i * groupLength, groupLength));
-        int remainLength = length % groupLength;
-        string remainString;
-        remainString.clear();
-        if (remainLength)
-            remainString = string(s, groupLength * group.size(), remainLength);
-        string result;
-        result.clear();
-        for (int g = 0; g < group.size(); ++g)
-            result.push_back(group[g][0]);
-        if (remainLength)
-            result.push_back(remainString[0]);
-        for (int i = 1; i < halfGroupLength; ++i)
-        {
-            int second = groupLength - i;
-            for (int g = 0; g < group.size(); ++g)
+        const int size = (numRows - 1) << 1;
+        const int groups = s.length() / size;
+        result = "";
+        for (int i = 0; i <= groups; ++i)
+            addChar(i * size, s);
+        for (int j = 1; j < numRows - 1; ++j)
+            for (int i = 0; i <= groups; ++i)
             {
-                result.push_back(group[g][i]);
-                result.push_back(group[g][second]);
+                addChar(i * size + j, s);
+                addChar((i + 1) * size - j, s);
             }
-            if (remainLength > i)
-                result.push_back(remainString[i]);
-            if (remainLength > second)
-                result.push_back(remainString[second]);
-        }
-        for (int g = 0; g < group.size(); ++g)
-            result.push_back(group[g][halfGroupLength]);
-        if (remainLength >= numRows)
-            result.push_back(remainString[halfGroupLength]);
+        for (int i = 0; i <= groups; ++i)
+            addChar(i * size + numRows - 1, s);
         return result;
     }
 };
