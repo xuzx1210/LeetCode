3e63ddc987e2aefe87d5994cba795174d463143b
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-02-06 04:18:20 +0800

    Update 438. Find All Anagrams in a String.cpp

--- 438. Find All Anagrams in a String.cpp
+++ 438. Find All Anagrams in a String.cpp
@@ -1,22 +1,46 @@
 class Solution
 {
+private:
+    size_t mismatch;
+    void insert(int &count)
+    {
+        if (count == 0)
+            ++mismatch;
+        ++count;
+        if (count == 0)
+            --mismatch;
+    }
+    void remove(int &count)
+    {
+
+        if (count == 0)
+            ++mismatch;
+        --count;
+        if (count == 0)
+            --mismatch;
+    }
+
 public:
     vector<int> findAnagrams(string s, string p)
     {
-        const int sLength = s.length(), pLength = p.length();
+        const size_t sLength = s.length(), pLength = p.length();
+        vector<int> result{};
         if (sLength < pLength)
-            return {};
-        vector<int> counter(26, 0), result{};
-        for (char c : p)
-            ++counter[c - 'a'];
-        for (int start = 0, end = 0; end < sLength; ++end)
+            return result;
+        array<int, 26> counter = {0};
+        mismatch = 0;
+        for (size_t i = 0; i < pLength; ++i)
+            remove(counter[p[i] - 'a']);
+        for (size_t i = 0; i < pLength; ++i)
+            insert(counter[s[i] - 'a']);
+        if (mismatch == 0)
+            result.emplace_back(0);
+        for (size_t i = pLength; i < sLength; ++i)
         {
-            int &cur = counter[s[end] - 'a'];
-            --cur;
-            while (cur < 0)
-                ++counter[s[start++] - 'a'];
-            if (end - start + 1 == pLength)
-                result.emplace_back(start);
+            insert(counter[s[i] - 'a']);
+            remove(counter[s[i - pLength] - 'a']);
+            if (mismatch == 0)
+                result.emplace_back(i + 1 - pLength);
         }
         return result;
     }
