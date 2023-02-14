1ed6063cf3f13cac9a8acd540f822ba29e6014cb
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-02-14 09:15:26 +0800

    Update 67. Add Binary.cpp

--- 67. Add Binary.cpp
+++ 67. Add Binary.cpp
@@ -1,28 +1,31 @@
 class Solution
 {
-public:
-    char addDigit(char a, char b, int &carry)
+private:
+    char fullAdder(char aIn, char bIn, bool &carry)
     {
-        int sum = a + b - ('0' << 1) + carry;
-        carry = sum >> 1;
-        return (sum & 1) + '0';
+        bool a = aIn - '0', b = bIn - '0';
+        bool c = a ^ b ^ carry;
+        carry = a && b || a && carry || b && carry;
+        return c + '0';
     }
+
+public:
     string addBinary(string a, string b)
     {
         reverse(a.begin(), a.end());
         reverse(b.begin(), b.end());
-        int aLength = a.length(), bLength = b.length();
-        int minLength = min(aLength, bLength);
+        const int aLength = a.length(), bLength = b.length();
+        const int minLength = min(aLength, bLength);
+        bool carry = false;
         string result("");
-        int carry = 0;
         for (int i = 0; i < minLength; ++i)
-            result.push_back(addDigit(a[i], b[i], carry));
+            result.push_back(fullAdder(a[i], b[i], carry));
         if (aLength < bLength)
-            for (int i = minLength; i < bLength; ++i)
-                result.push_back(addDigit('0', b[i], carry));
+            for (int i = aLength; i < bLength; ++i)
+                result.push_back(fullAdder('0', b[i], carry));
         else
-            for (int i = minLength; i < aLength; ++i)
-                result.push_back(addDigit(a[i], '0', carry));
+            for (int i = bLength; i < aLength; ++i)
+                result.push_back(fullAdder(a[i], '0', carry));
         if (carry)
             result.push_back('1');
         reverse(result.begin(), result.end());
