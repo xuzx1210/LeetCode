b16222ee426e365a8e7a4e9e99e7a32af581ea38
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-02-20 20:13:10 +0800

    Update 35. Search Insert Position.cpp

--- 35. Search Insert Position.cpp
+++ 35. Search Insert Position.cpp
@@ -3,17 +3,15 @@ class Solution
 public:
     int searchInsert(vector<int> &nums, int target)
     {
-        int left = 0, right = nums.size(), mid;
+        int left = 0, right = nums.size();
         while (left < right)
         {
-            mid = (left + right) >> 1;
-            if (target < nums[mid])
-                right = mid;
-            else if (nums[mid] < target)
-                left = mid + 1;
+            const int middle = (left + right) >> 1;
+            if (nums[middle] < target)
+                left = middle + 1;
             else
-                return mid;
+                right = middle;
         }
-        return left;
+        return right;
     }
 };
