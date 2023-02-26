ce4e199ebd530ab7db632b059d743b7cbcf883c3
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-02-27 03:53:15 +0800

    Update 72. Edit Distance.cpp

--- 72. Edit Distance.cpp
+++ 72. Edit Distance.cpp
@@ -3,18 +3,23 @@ class Solution
 public:
     int minDistance(string word1, string word2)
     {
-        int m = word1.length(), n = word2.length();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-        for (int i = 0; i <= m; ++i)
-            dp[i][0] = i;
-        for (int i = 0; i <= n; ++i)
-            dp[0][i] = i;
-        for (int i = 1; i <= m; ++i)
-            for (int j = 1; j <= n; ++j)
-                if (word1[i - 1] == word2[j - 1])
-                    dp[i][j] = dp[i - 1][j - 1];
+        const int length1 = word1.length(), length2 = word2.length();
+        vector<int> dp(length2 + 1);
+        iota(dp.begin(), dp.end(), 0);
+        for (int index1 = 1; index1 <= length1; ++index1) // pre     cur
+        {                                                 // dp[i-1] dp[i]
+            int pre = dp[0];
+            dp[0] = index1;
+            for (int index2 = 1; index2 <= length2; ++index2)
+            {
+                const int cur = dp[index2];
+                if (word1[index1 - 1] == word2[index2 - 1])
+                    dp[index2] = pre;
                 else
-                    dp[i][j] = 1 + min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1]));
-        return dp[m][n];
+                    dp[index2] = min(pre, min(dp[index2 - 1], cur)) + 1;
+                pre = cur;
+            }
+        }
+        return dp[length2];
     }
 };
