e5201b85d352dfc4dd87a0418320fcff7a57cce2
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-02-27 23:23:59 +0800

    Update 427. Construct Quad Tree.cpp

--- 427. Construct Quad Tree.cpp
+++ 427. Construct Quad Tree.cpp
@@ -1,23 +1,27 @@
 class Solution
 {
+private:
+    Node *dfs(const vector<vector<int>> &grid, const int x, const int y, const int n)
+    {
+        if (n == 1)
+            return new Node(grid[x][y], true);
+        const int half = n >> 1;
+        Node *topLeft = dfs(grid, x, y, half), *topRight = dfs(grid, x, y + half, half), *bottomLeft = dfs(grid, x + half, y, half), *bottomRight = dfs(grid, x + half, y + half, half);
+        if (topLeft->isLeaf && topRight->isLeaf && bottomLeft->isLeaf && bottomRight->isLeaf && topLeft->val == topRight->val && topRight->val == bottomLeft->val && bottomLeft->val == bottomRight->val)
+        {
+            Node *result = new Node(topLeft->val, true);
+            delete topLeft;
+            delete topRight;
+            delete bottomLeft;
+            delete bottomRight;
+            return result;
+        }
+        return new Node(false, false, topLeft, topRight, bottomLeft, bottomRight);
+    }
+
 public:
     Node *construct(vector<vector<int>> &grid)
     {
-        const int n = grid.size();
-        int sum = 0;
-        for (vector<int> &row : grid)
-            sum = accumulate(row.begin(), row.end(), sum);
-        if (!sum || sum == n * n)
-            return new Node(sum, true);
-        const int half = n >> 1;
-        vector<vector<int>> topLeft{}, topRight{}, bottomLeft{}, bottomRight{};
-        for (int i = 0; i < half; ++i)
-        {
-            topLeft.emplace_back(grid[i].begin(), grid[i].begin() + half);
-            topRight.emplace_back(grid[i].begin() + half, grid[i].end());
-            bottomLeft.emplace_back(grid[i + half].begin(), grid[i + half].begin() + half);
-            bottomRight.emplace_back(grid[i + half].begin() + half, grid[i + half].end());
-        }
-        return new Node(false, false, construct(topLeft), construct(topRight), construct(bottomLeft), construct(bottomRight));
+        return dfs(grid, 0, 0, grid.size());
     }
 };
