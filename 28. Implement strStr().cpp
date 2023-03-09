adca80d73309acab62b36c9bca74f86fc88677cd
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-03-09 08:49:15 +0800

    Update 28. Implement strStr().cpp

--- 28. Implement strStr().cpp
+++ 28. Implement strStr().cpp
@@ -3,6 +3,29 @@ class Solution
 public:
     int strStr(string haystack, string needle)
     {
-        return haystack.find(needle);
+        const int haystackLength = haystack.length(), needleLength = needle.length();
+        vector<int> failure(needleLength, 0);
+        for (int index = 1, length = 0; index < needleLength;)
+            if (needle[length] == needle[index])
+                failure[index++] = ++length;
+            else if (length)
+                length = failure[length - 1];
+            else
+                failure[index++] = 0;
+        for (int haystackIndex = 0, needleIndex = 0; haystackIndex < haystackLength;)
+        {
+            if (haystack[haystackIndex] == needle[needleIndex])
+            {
+                ++haystackIndex;
+                ++needleIndex;
+            }
+            else if (needleIndex)
+                needleIndex = failure[needleIndex - 1];
+            else
+                ++haystackIndex;
+            if (needleIndex == needleLength)
+                return haystackIndex - needleIndex;
+        }
+        return -1;
     }
 };
