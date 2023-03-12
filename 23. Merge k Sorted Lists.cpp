4c5cc8d9417c2aa56bb56af9f0fe5aff4d898175
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-03-12 09:57:06 +0800

    Update 23. Merge k Sorted Lists.cpp

--- 23. Merge k Sorted Lists.cpp
+++ 23. Merge k Sorted Lists.cpp
@@ -3,17 +3,25 @@ class Solution
 public:
     ListNode *mergeKLists(vector<ListNode *> &lists)
     {
-        priority_queue<int, vector<int>, greater<int>> pq;
-        for (auto head : lists)
-            for (ListNode *cur = head; cur != nullptr; cur = cur->next)
-                pq.push(cur->val);
-        ListNode *result = new ListNode;
-        for (ListNode *cur = result; !pq.empty(); pq.pop())
+        priority_queue<pair<int, ListNode *>, vector<pair<int, ListNode *>>, greater<pair<int, ListNode *>>> pq{}; // sort by val
+        for (ListNode *listHead : lists)
+            if (listHead) // skip empty list
+                pq.emplace(listHead->val, listHead);
+        ListNode *result = new ListNode(); // sentinel
+        ListNode *tail = result;           // tail of result list
+        while (!pq.empty())
         {
-            ListNode *tmp = new ListNode(pq.top());
-            cur->next = tmp;
-            cur = tmp;
+            ListNode *cur = pq.top().second;
+            pq.pop();
+            tail->next = cur; // push cur to the end of result list
+            tail = cur;
+            cur = cur->next;
+            if (cur)
+                pq.emplace(cur->val, cur);
         }
-        return result->next;
+        tail = result;
+        result = result->next;
+        delete tail; // release sentinel memory
+        return result;
     }
 };
