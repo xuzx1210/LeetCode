e4f93ef38ab059ff2e097833482c6837a4242e4e
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-03-12 14:05:39 +0800

    Update 23. Merge k Sorted Lists.cpp

--- 23. Merge k Sorted Lists.cpp
+++ 23. Merge k Sorted Lists.cpp
@@ -1,27 +1,37 @@
 class Solution
 {
+private:
+    void link(ListNode *&tail, ListNode *&target)
+    {
+        tail->next = target;
+        tail = target;
+        target = target->next;
+    }
+    ListNode *merge2Lists(const vector<ListNode *>::const_iterator begin, const vector<ListNode *>::const_iterator end)
+    {
+        if (begin == end)
+            return nullptr;
+        const int diff = end - begin;
+        if (diff == 1)
+            return *begin;
+        const vector<ListNode *>::const_iterator middle = begin + (diff >> 1);
+        ListNode sentinel;
+        ListNode *tail = &sentinel, *left = merge2Lists(begin, middle), *right = merge2Lists(middle, end);
+        while (true)
+            if (left && right)
+                link(tail, left->val < right->val ? left : right);
+            else if (left)
+                link(tail, left);
+            else if (right)
+                link(tail, right);
+            else
+                break;
+        return sentinel.next;
+    }
+
 public:
     ListNode *mergeKLists(vector<ListNode *> &lists)
     {
-        priority_queue<pair<int, ListNode *>, vector<pair<int, ListNode *>>, greater<pair<int, ListNode *>>> pq{}; // sort by val
-        for (ListNode *listHead : lists)
-            if (listHead) // skip empty list
-                pq.emplace(listHead->val, listHead);
-        ListNode *result = new ListNode(); // sentinel
-        ListNode *tail = result;           // tail of result list
-        while (!pq.empty())
-        {
-            ListNode *cur = pq.top().second;
-            pq.pop();
-            tail->next = cur; // push cur to the end of result list
-            tail = cur;
-            cur = cur->next;
-            if (cur)
-                pq.emplace(cur->val, cur);
-        }
-        tail = result;
-        result = result->next;
-        delete tail; // release sentinel memory
-        return result;
+        return merge2Lists(lists.begin(), lists.end());
     }
 };
