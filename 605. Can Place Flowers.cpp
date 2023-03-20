79c2c409ec18d7877190b986b6e8763025b03cfb
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-03-20 18:42:02 +0800

    Update 605. Can Place Flowers.cpp

--- 605. Can Place Flowers.cpp
+++ 605. Can Place Flowers.cpp
@@ -1,25 +1,18 @@
 class Solution
 {
-private:
-    bool place(const int index, vector<int> &flowerbed, int &n)
-    {
-        if (flowerbed[index - 1] || flowerbed[index] || flowerbed[index + 1])
-            return false;
-        flowerbed[index] = 1;
-        return !--n;
-    }
-
 public:
     bool canPlaceFlowers(vector<int> &flowerbed, int n)
     {
-        if (n == 0)
-            return true;
         const int flowerbedLength = flowerbed.size();
         flowerbed.emplace(flowerbed.begin(), 0);
         flowerbed.emplace_back(0);
-        for (int i = flowerbedLength; i; --i)
-            if (place(i, flowerbed, n))
-                return true;
-        return false;
+        for (int i = flowerbedLength; i && n; --i)
+        {
+            if (flowerbed[i - 1] || flowerbed[i] || flowerbed[i + 1])
+                continue;
+            flowerbed[i] = 1;
+            --n;
+        }
+        return !n;
     }
 };
