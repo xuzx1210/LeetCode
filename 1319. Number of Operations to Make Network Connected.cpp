c1551b95e7cd4b01e8ee62ee2bd8e784e4958054
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-03-23 13:26:20 +0800

    Create 1319. Number of Operations to Make Network Connected.cpp

---
+++ 1319. Number of Operations to Make Network Connected.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+private:
+    int groups;
+    vector<int> unionFind;
+    int find(int index)
+    {
+        if (unionFind[index] != index)
+            unionFind[index] = find(unionFind[index]);
+        return unionFind[index];
+    }
+    void join(int a, int b)
+    {
+        int findA = find(a), findB = find(b);
+        if (findA == findB)
+            return;
+        unionFind[findA] = findB;
+        --groups;
+    }
+
+public:
+    int makeConnected(int n, vector<vector<int>> &connections)
+    {
+        const int connectionsLength = connections.size();
+        if (connectionsLength < n - 1)
+            return -1;
+        unionFind.resize(n);
+        iota(unionFind.begin(), unionFind.end(), 0);
+        groups = n;
+        for (const vector<int> &connection : connections)
+            join(connection[0], connection[1]);
+        return groups - 1;
+    }
+};
