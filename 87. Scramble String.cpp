c3951eb09b10cf79094da67fdb05569ab5ab5f9d
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-03-30 16:14:19 +0800

    Update 87. Scramble String.cpp

--- 87. Scramble String.cpp
+++ 87. Scramble String.cpp
@@ -14,6 +14,9 @@ public:
             if (counter[i])
                 return false;
         vector<vector<vector<bool>>> dp(length + 1, vector<vector<bool>>(length, vector<bool>(length, false)));
+        // dimension 0: length of string, range: 1<=len<=length
+        // dimension 1: begin index of s1, range: 0<=i<length
+        // dimension 2: begin index of s2, range: 0<=j<length
         for (int i = 0; i < length; ++i)
             for (int j = 0; j < length; ++j)
                 dp[1][i][j] = s1[i] == s2[j];
@@ -22,7 +25,7 @@ public:
                 for (int j = 0; j + len <= length; ++j)
                     for (int left = 1, right = len - 1; left < len; ++left, --right)
                         if (dp[left][i][j] && dp[right][i + left][j + left] || dp[left][i][j + right] && dp[right][i + left][j])
-                        {
+                        { // xy or yx
                             dp[len][i][j] = true;
                             break;
                         }
