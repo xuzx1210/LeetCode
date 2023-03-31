c5bb3c0d6a39ef10b82603eb1f0fa896e00aaa9e
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-03-31 15:46:13 +0800

    Update 1444. Number of Ways of Cutting a Pizza.cpp

--- 1444. Number of Ways of Cutting a Pizza.cpp
+++ 1444. Number of Ways of Cutting a Pizza.cpp
@@ -1,34 +1,10 @@
 class Solution
 {
-private:
-    int rows, cols, divisor;
-    vector<vector<vector<int>>> dp; // result of [cuts][r][c]
-    vector<vector<int>> prefixSum;  // apples of bottom left
-    int dfs(int remainCuts, int r, int c)
-    {
-        if (prefixSum[r][c] <= remainCuts) // apples not enough for cuts
-            return 0;
-        if (remainCuts == 0) // no need to cut anymore
-            return 1;
-        if (dp[remainCuts][r][c] != -1) // if visited before
-            return dp[remainCuts][r][c];
-        int result = 0;
-        for (int cut = rows - 1; cut > r && prefixSum[cut][c] < prefixSum[r][c]; --cut)
-            result = (result + dfs(remainCuts - 1, cut, c)) % divisor;
-        for (int cut = cols - 1; cut > c && prefixSum[r][cut] < prefixSum[r][c]; --cut)
-            result = (result + dfs(remainCuts - 1, r, cut)) % divisor;
-        dp[remainCuts][r][c] = result; // store result for next time
-        return result;
-    }
-
 public:
     int ways(vector<string> &pizza, int k)
     {
-        rows = pizza.size();
-        cols = pizza.front().size();
-        divisor = 1000000007;
-        dp.resize(k, vector<vector<int>>(rows, vector<int>(cols, -1)));
-        prefixSum.resize(rows + 1, vector<int>(cols + 1, 0));
+        const int rows = pizza.size(), cols = pizza.front().size(), divisor = 1000000007;
+        vector<vector<int>> prefixSum(rows + 1, vector<int>(cols + 1, 0));
         for (int r = rows - 1; r >= 0; --r)
             for (int c = cols - 1; c >= 0; --c)
             {
@@ -36,6 +12,26 @@ public:
                 if (pizza[r][c] == 'A')
                     ++prefixSum[r][c];
             }
-        return dfs(k - 1, 0, 0);
+        vector<vector<int>> dp(rows, vector<int>(cols, 0));
+        for (int r = 0; r < rows; ++r)
+            for (int c = 0; c < cols; ++c)
+                if (prefixSum[r][c])
+                    dp[r][c] = 1;
+        for (int cuts = 1; cuts < k; ++cuts)
+        {
+            vector<vector<int>> tmp(rows, vector<int>(cols, 0));
+            for (int r = 0; r < rows; ++r)
+                for (int c = 0; c < cols; ++c)
+                {
+                    if (prefixSum[r][c] <= cuts)
+                        continue;
+                    for (int cut = rows - 1; cut > r && prefixSum[cut][c] < prefixSum[r][c]; --cut)
+                        tmp[r][c] = (tmp[r][c] + dp[cut][c]) % divisor;
+                    for (int cut = cols - 1; cut > c && prefixSum[r][cut] < prefixSum[r][c]; --cut)
+                        tmp[r][c] = (tmp[r][c] + dp[r][cut]) % divisor;
+                }
+            dp = tmp;
+        }
+        return dp.front().front();
     }
 };
