37e54f92b23e29e8bf7d112b74f18d0e0f3d6d2f
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-04-01 14:29:28 +0800

    Update 704. Binary Search.cpp

--- 704. Binary Search.cpp
+++ 704. Binary Search.cpp
@@ -3,17 +3,16 @@ class Solution
 public:
     int search(vector<int> &nums, int target)
     {
-        int left = 0, right = nums.size();
-        while (left < right)
+        for (int left = 0, right = nums.size(); left < right;)
         {
-            int mid = (left + right) >> 1;
-            int cur = nums[mid];
-            if (cur < target)
-                left = mid + 1;
-            else if (cur > target)
-                right = mid;
+            const int middle = (left + right) >> 1;
+            const int cur = nums[middle];
+            if (target < cur)
+                right = middle;
+            else if (cur < target)
+                left = middle + 1;
             else
-                return mid;
+                return middle;
         }
         return -1;
     }
