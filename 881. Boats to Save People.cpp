43ae65f77eb59d320d91177a8e3c5b7d9cff138f
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-04-03 15:06:29 +0800

    Update 881. Boats to Save People.cpp

--- 881. Boats to Save People.cpp
+++ 881. Boats to Save People.cpp
@@ -4,14 +4,10 @@ public:
     int numRescueBoats(vector<int> &people, int limit)
     {
         sort(people.begin(), people.end());
-        int left = 0, right = people.size() - 1, result = 0;
-        while (left <= right)
-        {
-            ++result;
+        int result = 0;
+        for (int left = 0, right = people.size() - 1; left <= right; --right, ++result)
             if (people[left] + people[right] <= limit)
                 ++left;
-            --right;
-        }
         return result;
     }
 };
