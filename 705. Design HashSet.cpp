ebb2d02e5ab3a1da34aee1c252a2d2685cb7d4e6
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-05-30 15:45:35 +0800

    Update 705. Design HashSet.cpp

--- 705. Design HashSet.cpp
+++ 705. Design HashSet.cpp
@@ -19,3 +19,36 @@ public:
         return hashset[key];
     }
 };
+class MyHashSet
+{
+private:
+    int prime;
+    vector<list<int>> table;
+    list<int>::iterator position;
+    void search(const int h, const int key)
+    {
+        position = find(table[h].begin(), table[h].end(), key);
+    }
+
+public:
+    MyHashSet() : prime(1009), table(prime, list<int>{}) {}
+    void add(int key)
+    {
+        if (contains(key))
+            return;
+        table[key % prime].emplace_back(key);
+    }
+    void remove(int key)
+    {
+        if (!contains(key))
+            return;
+        const int h = key % prime;
+        table[h].erase(position);
+    }
+    bool contains(int key)
+    {
+        const int h = key % prime;
+        search(h, key);
+        return position != table[h].end();
+    }
+};
