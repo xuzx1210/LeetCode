be3b737475efb59cf2b6e0202ac23462bb310f0d
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-05-31 17:36:13 +0800

    Update 1396. Design Underground System.cpp

--- 1396. Design Underground System.cpp
+++ 1396. Design Underground System.cpp
@@ -1,27 +1,26 @@
 class UndergroundSystem
 {
+private:
+    unordered_map<string, unordered_map<string, pair<int, int>>> history;
+    unordered_map<int, pair<string, int>> customer;
+
 public:
-    map<int, pair<string, int>> checkInData;         //<id, <checkInStationName, checkInTime>>
-    map<pair<string, string>, pair<int, int>> times; //<<checkInStationName, checkOutStationName>, <sum, num>>
-    UndergroundSystem()
-    {
-        checkInData.clear();
-        times.clear();
-    }
+    UndergroundSystem() : history{}, customer{} {}
     void checkIn(int id, string stationName, int t)
     {
-        checkInData[id] = {stationName, t};
+        customer[id] = {stationName, t};
     }
     void checkOut(int id, string stationName, int t)
     {
-        auto &data = checkInData[id];
-        auto &history = times[{data.first, stationName}];
-        history.first += (t - data.second);
-        ++history.second;
+        const auto &[startStation, startTime] = customer[id];
+        auto &[totalTime, count] = history[startStation][stationName];
+        const int time = t - startTime;
+        totalTime += time;
+        ++count;
     }
     double getAverageTime(string startStation, string endStation)
     {
-        auto &history = times[{startStation, endStation}];
-        return (double)history.first / history.second;
+        const auto &[totalTime, count] = history[startStation][endStation];
+        return (double)totalTime / count;
     }
 };
