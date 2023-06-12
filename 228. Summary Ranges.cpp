6e50c7c39a46b69c7020502df6caee723db3f0e0
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-06-12 16:46:00 +0800

    Update 228. Summary Ranges.cpp

--- 228. Summary Ranges.cpp
+++ 228. Summary Ranges.cpp
@@ -1,32 +1,19 @@
 class Solution
 {
 public:
-    void push(vector<string> &result, vector<int> &nums, int left, int right)
-    {
-        if (right == left + 1)
-            result.push_back(to_string(nums[left]));
-        else
-            result.push_back(to_string(nums[left]) + "->" + to_string(nums[right - 1]));
-    }
     vector<string> summaryRanges(vector<int> &nums)
     {
-        vector<string> result({});
-        if (nums.empty())
-            return result;
-        if (nums.size() == 1)
+        const int size = nums.size();
+        vector<string> result{};
+        for (int begin = 0, end = 1; end <= size; ++end)
         {
-            result.push_back(to_string(nums[0]));
-            return result;
-        }
-        int left = 0, right = 1;
-        for (; right < nums.size(); ++right)
-        {
-            if (nums[right] == nums[right - 1] + 1)
+            if (end != size && nums[end] - 1 == nums[end - 1])
                 continue;
-            push(result, nums, left, right);
-            left = right;
+            result.emplace_back(to_string(nums[begin]));
+            if (begin < end - 1)
+                result.back() += "->" + to_string(nums[end - 1]);
+            begin = end;
         }
-        push(result, nums, left, right);
         return result;
     }
 };
