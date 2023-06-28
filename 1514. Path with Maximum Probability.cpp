6c6015c640c45d44cc14e230879ed6506614e825
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-06-28 08:43:02 +0800

    Update 1514. Path with Maximum Probability.cpp

--- 1514. Path with Maximum Probability.cpp
+++ 1514. Path with Maximum Probability.cpp
@@ -4,32 +4,29 @@ public:
     double maxProbability(int n, vector<vector<int>> &edges, vector<double> &succProb, int start, int end)
     {
         vector<vector<pair<int, double>>> graph(n, vector<pair<int, double>>{});
-        for (int i = edges.size() - 1; i >= 0; --i)
+        for (int i = 0; i < edges.size(); ++i)
         {
-            graph[edges[i][0]].emplace_back(edges[i][1], succProb[i]);
-            graph[edges[i][1]].emplace_back(edges[i][0], succProb[i]);
+            const vector<int> &edge = edges[i];
+            const int a = edge[0], b = edge[1];
+            const double probability = succProb[i];
+            graph[a].emplace_back(b, probability);
+            graph[b].emplace_back(a, probability);
         }
-        priority_queue<pair<double, int>> border{};
-        border.emplace(1.0, start);
-        vector<bool> expanded(n, false);
-        vector<double> probability(n, 0.0);
-        probability[start] = 1.0;
-        while (!border.empty())
+        vector<bool> visited(n, false);
+        priority_queue<pair<double, int>> dijkstra{};
+        dijkstra.emplace(1.0, start);
+        while (!dijkstra.empty())
         {
-            const double toCur = border.top().first;
-            const int cur = border.top().second;
-            border.pop();
-            if (cur == end)
-                return toCur;
-            if (expanded[cur])
+            const auto [startToCurrProbability, curr] = dijkstra.top();
+            dijkstra.pop();
+            if (curr == end)
+                return startToCurrProbability;
+            if (visited[curr])
                 continue;
-            expanded[cur] = true;
-            for (const auto &[next, prob] : graph[cur])
-            {
-                probability[next] = max(probability[next], toCur * prob);
-                border.emplace(probability[next], next);
-            }
+            visited[curr] = true;
+            for (const auto &[next, currToNextProbability] : graph[curr])
+                dijkstra.emplace(startToCurrProbability * currToNextProbability, next);
         }
-        return probability[end];
+        return 0.0;
     }
 };
