86f40ae4365235f31a7ebe01c4abd4835822ee4e
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-07-09 04:11:28 +0800

    Update 2551. Put Marbles in Bags.cpp

--- 2551. Put Marbles in Bags.cpp
+++ 2551. Put Marbles in Bags.cpp
@@ -3,16 +3,17 @@ class Solution
 public:
     long long putMarbles(vector<int> &weights, int k)
     {
+        long long result = 0;
         const int size = weights.size() - 1;
+        --k;
         for (int i = 0; i < size; ++i)
             weights[i] += weights[i + 1];
         weights.pop_back();
-        long long result = 0;
-        nth_element(weights.begin(), weights.begin() + k - 1, weights.end());
-        for (int i = 0; i < k - 1; ++i)
+        nth_element(weights.begin(), weights.begin() + k, weights.end());
+        for (int i = 0; i < k; ++i)
             result -= weights[i];
-        nth_element(weights.begin(), weights.begin() + size - k + 1, weights.end());
-        for (int i = size - k + 1; i < size; ++i)
+        nth_element(weights.begin(), weights.begin() + size - k, weights.end());
+        for (int i = size - k; i < size; ++i)
             result += weights[i];
         return result;
     }
