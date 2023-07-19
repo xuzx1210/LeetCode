9e5c775d0ca853404d548f749a4fd3160723db0e
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-07-19 15:01:13 +0800

    Update 435. Non-overlapping Intervals.cpp

--- 435. Non-overlapping Intervals.cpp
+++ 435. Non-overlapping Intervals.cpp
@@ -3,19 +3,47 @@ class Solution
 public:
     int eraseOverlapIntervals(vector<vector<int>> &intervals)
     {
-        sort(intervals.begin(), intervals.end());
         int result = 0;
-        size_t previous = 0;
-        const size_t size = intervals.size();
-        for (size_t i = 1; i < size; ++i)
-            if (intervals[i][0] < intervals[previous][1])
+        // find minStartTime and maxEndTime
+        int minStartTime = INT_MAX, maxStartTime = INT_MIN;
+        for (const vector<int> &interval : intervals)
+        {
+            minStartTime = min(minStartTime, interval[0]);
+            maxStartTime = max(maxStartTime, interval[0]);
+        }
+        // shift minStartTime to 0
+        for (vector<int> &interval : intervals)
+        {
+            interval[0] -= minStartTime;
+            interval[1] -= minStartTime;
+        }
+        maxStartTime -= minStartTime;
+        // index: start time, data: end time
+        vector<int> startToEnd(maxStartTime + 1, -1);
+        for (const vector<int> &interval : intervals)
+        {
+            const int start = interval[0], end = interval[1];
+            if (startToEnd[start] == -1)
+                startToEnd[start] = end;
+            else // same start time
             {
+                startToEnd[start] = min(startToEnd[start], end); // save the earlier end time
                 ++result;
-                if (intervals[i][1] < intervals[previous][1])
-                    previous = i;
             }
-            else
-                previous = i;
+        }
+        for (int prev = 0, curr = 1; curr <= maxStartTime; ++curr)
+        {
+            if (startToEnd[curr] == -1)
+                continue;
+            if (startToEnd[prev] <= curr)
+            {
+                prev = curr;
+                continue;
+            }
+            ++result;
+            if (startToEnd[curr] < startToEnd[prev])
+                prev = curr;
+        }
         return result;
     }
 };
