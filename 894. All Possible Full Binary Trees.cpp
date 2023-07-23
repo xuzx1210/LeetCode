68503b8d412287ceafc373e33a09b43f16f3c918
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-07-23 15:26:54 +0800

    Create 894. All Possible Full Binary Trees.cpp

---
+++ 894. All Possible Full Binary Trees.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    vector<TreeNode *> allPossibleFBT(int n)
+    {
+        vector<vector<TreeNode *>> roots(n + 1, vector<TreeNode *>{});
+        roots[1].emplace_back(new TreeNode());
+        for (int i = 2; i <= n; ++i)
+            for (int left = 1, right = i - left - 1; right >= 1; ++left, --right)
+                for (TreeNode *leftTree : roots[left])
+                    for (TreeNode *rightTree : roots[right])
+                        roots[i].emplace_back(new TreeNode(0, leftTree, rightTree));
+        return roots.back();
+    }
+};
