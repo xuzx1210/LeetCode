9239331a1c91068804e86ebed329b2f4a8e33c20
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-07-24 13:51:29 +0800

    Update 50. Pow(x, n).cpp

--- 50. Pow(x, n).cpp
+++ 50. Pow(x, n).cpp
@@ -1,8 +1,21 @@
 class Solution
 {
+private:
+    double binaryPow(double x, long long n)
+    {
+        if (n == 0)
+            return 1.0;
+        const double half = myPow(x, n >> 1);
+        double result = half * half;
+        if (n & 1)
+            result *= x;
+        return result;
+    }
+
 public:
     double myPow(double x, int n)
     {
-        return pow(x, n);
+        long long power = n;
+        return power < 0 ? 1.0 / binaryPow(x, -power) : binaryPow(x, power);
     }
 };
