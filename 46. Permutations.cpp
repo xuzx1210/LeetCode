3fc9e3f0e4d18726dd0d07809a492f8355485150
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-08-02 16:17:20 +0800

    Update 46. Permutations.cpp

--- 46. Permutations.cpp
+++ 46. Permutations.cpp
@@ -1,12 +1,27 @@
 class Solution
 {
+private:
+    vector<vector<int>> result;
+    void recursive(const int index, vector<int> &nums)
+    {
+        if (index == nums.size())
+        {
+            result.emplace_back(nums);
+            return;
+        }
+        for (int i = index; i < nums.size(); ++i)
+        {
+            swap(nums[index], nums[i]);
+            recursive(index + 1, nums);
+            swap(nums[index], nums[i]);
+        }
+    }
+
 public:
     vector<vector<int>> permute(vector<int> &nums)
     {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> result(1, nums);
-        while (next_permutation(nums.begin(), nums.end()))
-            result.push_back(nums);
+        result.clear();
+        recursive(0, nums);
         return result;
     }
 };
