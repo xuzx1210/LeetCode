dc4b8ebf59d003331c622c2737b4a6ec13cd03d4
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-08-04 22:27:03 +0800

    Update 139. Word Break.cpp

--- 139. Word Break.cpp
+++ 139. Word Break.cpp
@@ -1,32 +1,24 @@
 class Solution
 {
 public:
-    bool equal(string &s, int index, string &word)
-    {
-        if (index + word.length() > s.length())
-            return false;
-        for (int i = 0; i < word.length(); ++i)
-            if (s[index + i] != word[i])
-                return false;
-        return true;
-    }
     bool wordBreak(string s, vector<string> &wordDict)
     {
-        int length = s.length();
-        vector<bool> dp(length + 1, false);
+        const int sLength = s.length();
+        vector<bool> dp(sLength + 1, false);
         dp[0] = true;
-        for (int i = 0; i < length; ++i)
+        for (int i = 0; i < sLength; ++i)
         {
             if (!dp[i])
                 continue;
-            for (string &word : wordDict)
+            for (const string &word : wordDict)
             {
-                if (dp[i + word.length()])
+                const int wordLength = word.length();
+                if (sLength < i + wordLength)
                     continue;
-                if (equal(s, i, word))
-                    dp[i + word.length()] = true;
+                if (s.substr(i, wordLength) == word)
+                    dp[i + wordLength] = true;
             }
         }
-        return dp[length];
+        return dp.back();
     }
 };
