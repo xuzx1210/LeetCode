8ce3b4e87fb09242a8d9015ece4720babd978136
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-08-06 23:12:27 +0800

    Update 920. Number of Music Playlists.cpp

--- 920. Number of Music Playlists.cpp
+++ 920. Number of Music Playlists.cpp
@@ -4,15 +4,11 @@ public:
     int numMusicPlaylists(int n, int goal, int k)
     {
         const long mod = 1000000007;
-        vector<vector<long>> dp(goal + 1, vector<long>(n + 1, 0));
-        dp[0][0] = 1;
-        for (int all = 1; all <= goal; ++all)
-            for (int used = 1; used <= min(all, n); ++used)
-            {
-                dp[all][used] = (dp[all - 1][used - 1] * (n - (used - 1))) % mod;
-                if (k < used)
-                    dp[all][used] = (dp[all][used] + dp[all - 1][used] * (used - k)) % mod;
-            }
-        return dp[goal][n];
+        vector<long> dp(n + 1, 0);
+        dp[1] = n;
+        for (int all = 2; all <= goal; ++all)
+            for (int used = min(all, n); used; --used)
+                dp[used] = (dp[used - 1] * (n - (used - 1)) + dp[used] * (used - k) * (k < used)) % mod;
+        return dp[n];
     }
 };
