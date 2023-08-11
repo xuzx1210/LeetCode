f15d2795ca5ea36a5473e326af966e70b163f9cf
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-08-11 17:13:08 +0800

    Update 81. Search in Rotated Sorted Array II.cpp

--- 81. Search in Rotated Sorted Array II.cpp
+++ 81. Search in Rotated Sorted Array II.cpp
@@ -3,6 +3,32 @@ class Solution
 public:
     bool search(vector<int> &nums, int target)
     {
-        return find(nums.begin(), nums.end(), target) != nums.end();
+        int left = 0, right = nums.size() - 1;
+        while (left <= right)
+        {
+            const int middle = (left + right) >> 1;
+            if (nums[middle] == target)
+                return true;
+            if (nums[left] == nums[middle] && nums[middle] == nums[right])
+            {
+                ++left;
+                --right;
+            }
+            else if (nums[left] <= nums[middle])
+            {
+                if (nums[left] <= target && target < nums[middle])
+                    right = middle - 1;
+                else
+                    left = middle + 1;
+            }
+            else
+            {
+                if (nums[middle] < target && target <= nums[right])
+                    left = middle + 1;
+                else
+                    right = middle - 1;
+            }
+        }
+        return false;
     }
 };
