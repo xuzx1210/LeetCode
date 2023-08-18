c697c533b0455509669674ce4bab11f8c3692230
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-08-18 08:54:45 +0800

    Update 1615. Maximal Network Rank.cpp

--- 1615. Maximal Network Rank.cpp
+++ 1615. Maximal Network Rank.cpp
@@ -4,41 +4,18 @@ public:
     int maximalNetworkRank(int n, vector<vector<int>> &roads)
     {
         vector<vector<bool>> graph(n, vector<bool>(n, false));
-        vector<int> degrees(n, 0);
+        vector<int> degree(n, 0);
         for (const vector<int> &road : roads)
         {
             const int a = road[0], b = road[1];
-            graph[a][b] = true;
-            graph[b][a] = true;
-            ++degrees[a];
-            ++degrees[b];
+            graph[a][b] = graph[b][a] = true;
+            ++degree[a];
+            ++degree[b];
         }
-        const int firstDegree = *max_element(degrees.begin(), degrees.end());
-        vector<int> firstDegreeCites{};
-        for (int city = 0; city < n; ++city)
-            if (degrees[city] == firstDegree)
-                firstDegreeCites.emplace_back(city);
-        const int firstDegreeCitesSize = firstDegreeCites.size();
-        if (2 <= firstDegreeCitesSize)
-        {
-            for (int i = 0; i < firstDegreeCitesSize; ++i)
-                for (int j = i + 1; j < firstDegreeCitesSize; ++j)
-                    if (!graph[firstDegreeCites[i]][firstDegreeCites[j]])
-                        return firstDegree << 1;
-            return (firstDegree << 1) - 1;
-        }
-        int secondDegree = 0;
-        for (const int degree : degrees)
-            if (degree < firstDegree)
-                secondDegree = max(secondDegree, degree);
-        vector<int> secondDegreeCites{};
-        for (int city = 0; city < n; ++city)
-            if (degrees[city] == secondDegree)
-                secondDegreeCites.emplace_back(city);
-        for (const int firstCity : firstDegreeCites)
-            for (const int secondCity : secondDegreeCites)
-                if (!graph[firstCity][secondCity])
-                    return firstDegree + secondDegree;
-        return firstDegree + secondDegree - 1;
+        int result = 0;
+        for (int i = 0; i < n; ++i)
+            for (int j = i + 1; j < n; ++j)
+                result = max(result, degree[i] + degree[j] - graph[i][j]);
+        return result;
     }
 };
