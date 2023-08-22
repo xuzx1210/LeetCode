2a3c79edd296fdd0e177ee9fc3bd6758842c9d8b
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-08-22 08:09:14 +0800

    Update 168. Excel Sheet Column Title.cpp

--- 168. Excel Sheet Column Title.cpp
+++ 168. Excel Sheet Column Title.cpp
@@ -3,8 +3,14 @@ class Solution
 public:
     string convertToTitle(int columnNumber)
     {
-        int tmp = columnNumber - 1;
-        div_t d = div(tmp, 26);
-        return columnNumber == 0 ? "" : convertToTitle(d.quot) + (char)(d.rem + 'A');
+        string result{};
+        while (columnNumber)
+        {
+            --columnNumber;
+            result.push_back(columnNumber % 26 + 'A');
+            columnNumber /= 26;
+        }
+        reverse(result.begin(), result.end());
+        return result;
     }
 };
