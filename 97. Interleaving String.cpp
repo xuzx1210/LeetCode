3b07e6c7cffa5356bd3dcade48b079f9021e23a2
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-08-25 08:59:37 +0800

    Update 97. Interleaving String.cpp

--- 97. Interleaving String.cpp
+++ 97. Interleaving String.cpp
@@ -3,18 +3,21 @@ class Solution
 public:
     bool isInterleave(string s1, string s2, string s3)
     {
-        int len1 = s1.length(), len2 = s2.length();
-        if (len1 + len2 != s3.length())
+        if (s1.length() < s2.length())
+            s1.swap(s2);
+        const int length1 = s1.length(), length2 = s2.length();
+        if (length1 + length2 != s3.length())
             return false;
-        vector<vector<bool>> dp(len1 + 1, vector<bool>(len2 + 1, false));
-        dp[0][0] = true;
-        for (int i = 1; i <= len1; ++i)
-            dp[i][0] = (s1[i - 1] == s3[i - 1]) && dp[i - 1][0];
-        for (int j = 1; j <= len2; ++j)
-            dp[0][j] = (s2[j - 1] == s3[j - 1]) && dp[0][j - 1];
-        for (int i = 1; i <= len1; ++i)
-            for (int j = 1; j <= len2; ++j)
-                dp[i][j] = dp[i - 1][j] && (s1[i - 1] == s3[i + j - 1]) || dp[i][j - 1] && (s2[j - 1] == s3[i + j - 1]);
-        return dp[len1][len2];
+        vector<bool> dp(length2 + 1, false);
+        dp[0] = true;
+        for (int j = 1; j <= length2; ++j)
+            dp[j] = dp[j - 1] && s2[j - 1] == s3[j - 1];
+        for (int i = 1; i <= length1; ++i)
+        {
+            dp[0] = dp[0] && s1[i - 1] == s3[i - 1];
+            for (int j = 1; j <= length2; ++j)
+                dp[j] = dp[j] && s1[i - 1] == s3[i + j - 1] || dp[j - 1] && s2[j - 1] == s3[i + j - 1];
+        }
+        return dp[length2];
     }
 };
