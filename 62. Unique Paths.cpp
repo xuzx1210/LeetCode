0771547d88755b9c00f8c9c21f9000ba536937ce
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-09-03 18:58:37 +0800

    Update 62. Unique Paths.cpp

--- 62. Unique Paths.cpp
+++ 62. Unique Paths.cpp
@@ -5,10 +5,14 @@ public:
     {
         if (m < n)
             swap(m, n);
-        vector<int> dp(n, 1);
-        for (int i = 1; i < m; ++i)
-            for (int j = 1; j < n; ++j)
-                dp[j] += dp[j - 1];
-        return dp.back();
+        --m;
+        --n;
+        long result = 1;
+        for (int i = 1; i <= n; ++i)
+        {
+            result *= (i + m);
+            result /= i;
+        }
+        return result;
     }
 };
