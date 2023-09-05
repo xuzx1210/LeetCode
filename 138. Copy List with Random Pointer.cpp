273e171738cc989811b613cd048cfbfb9a9696df
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-09-05 19:12:15 +0800

    Update 138. Copy List with Random Pointer.cpp

--- 138. Copy List with Random Pointer.cpp
+++ 138. Copy List with Random Pointer.cpp
@@ -5,28 +5,23 @@ public:
     {
         if (!head)
             return nullptr;
-        vector<Node *> list({});
+        for (Node *cur = head; cur; cur = cur->next->next)
+        {
+            Node *tmp = new Node(cur->val);
+            tmp->next = cur->next;
+            cur->next = tmp;
+        }
+        Node *result = head->next;
+        for (Node *cur = head; cur; cur = cur->next->next)
+            if (cur->random)
+                cur->next->random = cur->random->next;
         for (Node *cur = head; cur; cur = cur->next)
-            list.push_back(cur);
-        const int size = list.size();
-        vector<int> randomIndex(size, -1);
-        for (int i = 0; i < size; ++i)
-            randomIndex[i] = list[i]->random ? find(list.begin(), list.end(), list[i]->random) - list.begin() : -1;
-        vector<Node *> result(size, nullptr);
-        for (int i = 0; i < size; ++i)
         {
-            Node *tmp = new Node(list[i]->val);
-            result[i] = tmp;
+            Node *tmp = cur->next;
+            cur->next = tmp->next;
+            if (tmp->next)
+                tmp->next = tmp->next->next;
         }
-        for (int i = 0; i < size - 1; ++i)
-        {
-            result[i]->next = result[i + 1];
-            int index = randomIndex[i];
-            result[i]->random = index == -1 ? nullptr : result[index];
-        }
-        result.back()->next = nullptr;
-        int backIndex = randomIndex.back();
-        result.back()->random = backIndex == -1 ? nullptr : result[backIndex];
-        return result[0];
+        return result;
     }
 };
