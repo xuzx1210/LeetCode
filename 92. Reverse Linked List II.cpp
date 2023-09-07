cbd726e43a02913a2a717db9db7addd3a6206ac0
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-09-07 16:48:20 +0800

    Update 92. Reverse Linked List II.cpp

--- 92. Reverse Linked List II.cpp
+++ 92. Reverse Linked List II.cpp
@@ -3,21 +3,21 @@ class Solution
 public:
     ListNode *reverseBetween(ListNode *head, int left, int right)
     {
-        vector<ListNode *> list({});
-        for (ListNode *cur = head; cur != nullptr; cur = cur->next)
-            list.push_back(cur);
-        --left;
-        --right;
-        for (int i = right; i > left; --i)
-            list[i]->next = list[i - 1];
-        if (left)
-            list[left - 1]->next = list[right];
-        else
-            head = list[right];
-        if (right == list.size() - 1)
-            list[left]->next = nullptr;
-        else
-            list[left]->next = list[right + 1];
-        return head;
+        ListNode dummy;
+        dummy.next = head;
+        ListNode *sentinel, *tail = &dummy;
+        for (int i = 0; i < left; ++i)
+        {
+            sentinel = tail;
+            tail = tail->next;
+        }
+        for (int i = left; i < right; ++i)
+        {
+            ListNode *previous = tail->next;
+            tail->next = previous->next;
+            previous->next = sentinel->next;
+            sentinel->next = previous;
+        }
+        return dummy.next;
     }
 };
