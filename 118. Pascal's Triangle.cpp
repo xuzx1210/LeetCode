f31b5ee0a11e3434a9e323707bf762400e0285b2
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-09-08 18:47:44 +0800

    Update 118. Pascal's Triangle.cpp

--- 118. Pascal's Triangle.cpp
+++ 118. Pascal's Triangle.cpp
@@ -3,12 +3,13 @@ class Solution
 public:
     vector<vector<int>> generate(int numRows)
     {
-        vector<vector<int>> result(numRows, vector<int>({1}));
+        vector<vector<int>> result{{1}};
         for (int i = 1; i < numRows; ++i)
         {
+            result.emplace_back(result.back());
             for (int j = 1; j < i; ++j)
-                result[i].push_back(result[i - 1][j - 1] + result[i - 1][j]);
-            result[i].push_back(1);
+                result[i][j] += result[i - 1][j - 1];
+            result[i].emplace_back(1);
         }
         return result;
     }
