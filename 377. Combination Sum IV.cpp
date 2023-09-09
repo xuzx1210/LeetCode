9a1addccc2362e2f9be661ed744717495bc7666e
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-09-10 00:34:00 +0800

    Update 377. Combination Sum IV.cpp

--- 377. Combination Sum IV.cpp
+++ 377. Combination Sum IV.cpp
@@ -1,25 +1,17 @@
 class Solution
 {
-private:
-    int dfsAndMemoization(vector<int> &nums, int target, vector<int> &dp)
-    {
-        if (dp[target] > -1)
-            return dp[target];
-        dp[target] = 0;
-        for (int &num : nums)
-            if (num <= target)
-                dp[target] += dfsAndMemoization(nums, target - num, dp);
-            else
-                break;
-        return dp[target];
-    }
-
 public:
     int combinationSum4(vector<int> &nums, int target)
     {
         sort(nums.begin(), nums.end());
-        vector<int> dp(target + 1, -1);
+        vector<unsigned int> dp(target + 1, 0);
         dp[0] = 1;
-        return dfsAndMemoization(nums, target, dp);
+        for (int sum = 1; sum <= target; ++sum)
+            for (const int num : nums)
+                if (num <= sum)
+                    dp[sum] += dp[sum - num];
+                else
+                    break;
+        return dp[target];
     }
 };
