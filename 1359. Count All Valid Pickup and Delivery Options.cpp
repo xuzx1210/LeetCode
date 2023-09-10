ed9bf788bc2d2d6fe5a3496968ce66adc2749934
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-09-10 17:23:13 +0800

    Update 1359. Count All Valid Pickup and Delivery Options.cpp

--- 1359. Count All Valid Pickup and Delivery Options.cpp
+++ 1359. Count All Valid Pickup and Delivery Options.cpp
@@ -3,12 +3,10 @@ class Solution
 public:
     int countOrders(int n)
     {
-        vector<unsigned long long> result(n + 1, 1);
-        for (int i = 2; i <= n; ++i)
-        {
-            int options = (i - 1) << 1;
-            result[i] = (((options + 1) * (options + 2) >> 1) * result[i - 1]) % 1000000007;
-        }
-        return result[n];
+        const size_t mod = 1000000007;
+        size_t result = 1;
+        for (size_t i = 1, j = 1; i <= n; ++i, j += 2)
+            result = (result * i * j) % mod;
+        return result;
     }
 };
