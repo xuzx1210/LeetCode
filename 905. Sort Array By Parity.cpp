3f329d81dda1b547061079a927baec36d43b21b3
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-09-28 09:33:42 +0800

    Update 905. Sort Array By Parity.cpp

--- 905. Sort Array By Parity.cpp
+++ 905. Sort Array By Parity.cpp
@@ -3,14 +3,11 @@ class Solution
 public:
     vector<int> sortArrayByParity(vector<int> &nums)
     {
-        int left = 0, right = nums.size() - 1;
-        while (left < right)
-        {
-            if (~nums[right] & 1)
-                swap(nums[left++], nums[right]);
+        for (int left = 0, right = nums.size() - 1; left < right;)
+            if (nums[left] & 1 == 1)
+                swap(nums[left], nums[right--]);
             else
-                --right;
-        }
+                ++left;
         return nums;
     }
 };
