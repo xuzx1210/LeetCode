f276232197698dd07ae7b78f37f358418ed6daeb
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-09-30 23:09:57 +0800

    Update 456. 132 Pattern.cpp

--- 456. 132 Pattern.cpp
+++ 456. 132 Pattern.cpp
@@ -3,9 +3,8 @@ class Solution
 public:
     bool find132pattern(vector<int> &nums)
     {
-        int numsK = INT_MIN;
         stack<int> monotonic{};
-        for (int i = nums.size() - 1; i >= 0; --i)
+        for (int i = nums.size() - 1, numsK = INT_MIN; i >= 0; --i)
         {
             if (nums[i] < numsK)
                 return true;
