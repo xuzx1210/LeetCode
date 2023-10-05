4e2e7bfb220d8a385ef9007e1e2ae0297f38b7e3
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-10-05 16:44:02 +0800

    Update 229. Majority Element II.cpp

--- 229. Majority Element II.cpp
+++ 229. Majority Element II.cpp
@@ -3,14 +3,41 @@ class Solution
 public:
     vector<int> majorityElement(vector<int> &nums)
     {
-        unordered_map<int, int> table = {};
-        for (int num : nums)
-            ++table[num];
-        int threshold = nums.size() / 3;
-        vector<int> result = {};
-        for (auto entry : table)
-            if (entry.second > threshold)
-                result.push_back(entry.first);
+        vector<int> result{};
+        const int size = nums.size();
+        int candidate0, candidate1;
+        int count0 = 0, count1 = 0;
+        for (int i = 0; i < size; ++i)
+            if (nums[i] == candidate0)
+                ++count0;
+            else if (nums[i] == candidate1)
+                ++count1;
+            else if (count0 == 0)
+            {
+                candidate0 = nums[i];
+                count0 = 1;
+            }
+            else if (count1 == 0)
+            {
+                candidate1 = nums[i];
+                count1 = 1;
+            }
+            else
+            {
+                --count0;
+                --count1;
+            }
+        count0 = count1 = 0;
+        for (int i = 0; i < size; ++i)
+            if (nums[i] == candidate0)
+                ++count0;
+            else if (nums[i] == candidate1)
+                ++count1;
+        const int threshold = size / 3;
+        if (count0 > threshold)
+            result.emplace_back(candidate0);
+        if (count1 > threshold)
+            result.emplace_back(candidate1);
         return result;
     }
 };
