5c4ace4e5ee82034c3eba65ce99554622739682a
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-10-10 14:14:14 +0800

    Update 229. Majority Element II.cpp

--- 229. Majority Element II.cpp
+++ 229. Majority Element II.cpp
@@ -4,10 +4,9 @@ public:
     vector<int> majorityElement(vector<int> &nums)
     {
         vector<int> result{};
-        const int size = nums.size();
-        int candidate0, candidate1;
-        int count0 = 0, count1 = 0;
-        for (int i = 0; i < size; ++i)
+        const int n = nums.size(), threshold = n / 3;
+        int candidate0, count0 = 0, candidate1, count1 = 0;
+        for (int i = 0; i < n; ++i)
             if (nums[i] == candidate0)
                 ++count0;
             else if (nums[i] == candidate1)
@@ -28,15 +27,14 @@ public:
                 --count1;
             }
         count0 = count1 = 0;
-        for (int i = 0; i < size; ++i)
+        for (int i = 0; i < n; ++i)
             if (nums[i] == candidate0)
                 ++count0;
             else if (nums[i] == candidate1)
                 ++count1;
-        const int threshold = size / 3;
-        if (count0 > threshold)
+        if (threshold < count0)
             result.emplace_back(candidate0);
-        if (count1 > threshold)
+        if (threshold < count1)
             result.emplace_back(candidate1);
         return result;
     }
