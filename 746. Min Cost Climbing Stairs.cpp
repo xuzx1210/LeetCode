96a9f30b01176c116468bd36ec06b7c73cd0c9c1
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-10-13 11:01:26 +0800

    Update 746. Min Cost Climbing Stairs.cpp

--- 746. Min Cost Climbing Stairs.cpp
+++ 746. Min Cost Climbing Stairs.cpp
@@ -3,8 +3,10 @@ class Solution
 public:
     int minCostClimbingStairs(vector<int> &cost)
     {
-        for (int i = cost.size() - 3; i >= 0; --i)
-            cost[i] += min(cost[i + 1], cost[i + 2]);
-        return min(cost[0], cost[1]);
+        cost.emplace_back(0);
+        const int size = cost.size();
+        for (int i = 2; i < size; ++i)
+            cost[i] += min(cost[i - 2], cost[i - 1]);
+        return cost.back();
     }
 };
