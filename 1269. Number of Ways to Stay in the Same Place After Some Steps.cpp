7faaa78e184e00e769477fc1b4f3499dd7ff66bc
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-10-15 20:32:49 +0800

    Update 1269. Number of Ways to Stay in the Same Place After Some Steps.cpp

--- 1269. Number of Ways to Stay in the Same Place After Some Steps.cpp
+++ 1269. Number of Ways to Stay in the Same Place After Some Steps.cpp
@@ -5,21 +5,20 @@ public:
     {
         if (arrLen == 1)
             return 1;
-        arrLen = min(arrLen, steps);
+        arrLen = min(arrLen, (steps >> 1) + 1);
         const int mod = 1000000007;
         vector<int> dp(arrLen, 0);
         dp[0] = 1;
         for (int step = 0; step < steps; ++step)
         {
             vector<int> tmp(dp);
-            tmp[1] = (tmp[1] + dp[0]) % mod;
+            dp[1] = (dp[1] + tmp[0]) % mod;
             for (int i = 1; i <= arrLen - 2; ++i)
             {
-                tmp[i - 1] = (tmp[i - 1] + dp[i]) % mod;
-                tmp[i + 1] = (tmp[i + 1] + dp[i]) % mod;
+                dp[i - 1] = (dp[i - 1] + tmp[i]) % mod;
+                dp[i + 1] = (dp[i + 1] + tmp[i]) % mod;
             }
-            tmp[arrLen - 2] = (tmp[arrLen - 2] + dp[arrLen - 1]) % mod;
-            dp.swap(tmp);
+            dp[arrLen - 2] = (dp[arrLen - 2] + tmp[arrLen - 1]) % mod;
         }
         return dp[0];
     }
