6e82c13bb222673028ec4e089a0e4988e905d8a1
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-10-16 14:00:20 +0800

    Update 119. Pascal's Triangle II.cpp

--- 119. Pascal's Triangle II.cpp
+++ 119. Pascal's Triangle II.cpp
@@ -3,13 +3,10 @@ class Solution
 public:
     vector<int> getRow(int rowIndex)
     {
-        vector<int> result({1});
+        vector<int> result(rowIndex + 1, 1);
         for (int i = 0; i < rowIndex; ++i)
-        {
-            for (int j = result.size() - 1; j >= 1; --j)
+            for (int j = i; j >= 1; --j)
                 result[j] += result[j - 1];
-            result.push_back(1);
-        }
         return result;
     }
 };
