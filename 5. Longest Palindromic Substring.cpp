79f98d1c2c3d09f7de5a1e55c0ead710f54ebf19
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-10-27 08:29:41 +0800

    Update 5. Longest Palindromic Substring.cpp

--- 5. Longest Palindromic Substring.cpp
+++ 5. Longest Palindromic Substring.cpp
@@ -1,21 +1,32 @@
 class Solution
 {
+private:
+    string result;
+    int sLength;
+    int maximum;
+    void findPalindrome(int left, int right, string &s)
+    {
+        for (; 0 <= left && right < sLength; --left, ++right)
+            if (s[left] != s[right])
+                break;
+        const int length = right - left - 1;
+        if (length <= maximum)
+            return;
+        maximum = length;
+        result = s.substr(left + 1, length);
+    }
+
 public:
     string longestPalindrome(string s)
     {
-        int n = s.length();
-        if (n == 1)
-            return s;
-        vector<vector<bool>> dp(n, vector<bool>(n + 1));
-        for (int i = 0; i < n; ++i)
-            dp[i][0] = dp[i][1] = true;
-        for (int length = 2; length <= n; ++length)
-            for (int start = 0; start <= n - length; ++start)
-                dp[start][length] = dp[start + 1][length - 2] && s[start] == s[start + length - 1];
-        for (int length = n; length >= 0; --length)
-            for (int start = 0; start <= n - length; ++start)
-                if (dp[start][length])
-                    return s.substr(start, length);
-        return s;
+        result.clear();
+        sLength = s.length();
+        maximum = 0;
+        for (int i = 0; i < sLength; ++i)
+        {
+            findPalindrome(i, i, s);
+            findPalindrome(i, i + 1, s);
+        }
+        return result;
     }
 };
