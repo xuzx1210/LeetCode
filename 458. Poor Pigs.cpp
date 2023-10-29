bca405c39edc6f5e225c9bd24a178e172ea8afac
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-10-30 00:26:44 +0800

    Create 458. Poor Pigs.cpp

---
+++ 458. Poor Pigs.cpp
@@ -0,0 +1,8 @@
+class Solution
+{
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest)
+    {
+        return ceil(log2(buckets) / log2(minutesToTest / minutesToDie + 1));
+    }
+};
