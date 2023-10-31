94b5e26fa81ec568eb2efeee914e25795a7cf485
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-11-01 02:19:31 +0800

    Update 2433. Find The Original Array of Prefix Xor.cpp

--- 2433. Find The Original Array of Prefix Xor.cpp
+++ 2433. Find The Original Array of Prefix Xor.cpp
@@ -3,11 +3,8 @@ class Solution
 public:
     vector<int> findArray(vector<int> &pref)
     {
-        const int n = pref.size();
-        vector<int> arr(n);
-        arr[0] = pref[0];
-        for (int i = 1; i < n; ++i)
-            arr[i] = pref[i - 1] ^ pref[i];
-        return arr;
+        for (int i = pref.size() - 1; i; --i)
+            pref[i] ^= pref[i - 1];
+        return pref;
     }
 };
