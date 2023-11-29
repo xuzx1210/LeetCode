ce05295facf228e0c90eac2a821ca138243db3bf
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-11-29 15:29:43 +0800

    Update 191. Number of 1 Bits.cpp

--- 191. Number of 1 Bits.cpp
+++ 191. Number of 1 Bits.cpp
@@ -4,11 +4,8 @@ public:
     int hammingWeight(uint32_t n)
     {
         int result = 0;
-        while (n)
-        {
+        for (; n; n &= (n - 1))
             ++result;
-            n &= (n - 1);
-        }
         return result;
     }
 };
