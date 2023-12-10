ecc0d6ec3fc2e4a4e6d484a5655e6c4ea32d29f1
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-12-10 08:31:29 +0800

    Update 867. Transpose Matrix.cpp

--- 867. Transpose Matrix.cpp
+++ 867. Transpose Matrix.cpp
@@ -3,7 +3,7 @@ class Solution
 public:
     vector<vector<int>> transpose(vector<vector<int>> &matrix)
     {
-        int m = matrix.size(), n = matrix.front().size();
+        const int m = matrix.size(), n = matrix.front().size();
         vector<vector<int>> result(n, vector<int>(m));
         for (int i = 0; i < m; ++i)
             for (int j = 0; j < n; ++j)
