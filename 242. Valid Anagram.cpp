f925e6d163a5884c3159a35f9f91c4395208a144
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-12-16 13:25:02 +0800

    Update 242. Valid Anagram.cpp

--- 242. Valid Anagram.cpp
+++ 242. Valid Anagram.cpp
@@ -3,14 +3,16 @@ class Solution
 public:
     bool isAnagram(string s, string t)
     {
-        unordered_map<char, int> letters = {};
-        for (char &c : s)
-            ++letters[c];
-        for (char &c : t)
-            --letters[c];
-        bool result = true;
-        for (auto &p : letters)
-            result = result && !p.second;
-        return result;
+        if (s.length() != t.length())
+            return false;
+        vector<int> counts(26, 0);
+        for (const char c : s)
+            ++counts[c - 'a'];
+        for (const char c : t)
+            --counts[c - 'a'];
+        for (const int count : counts)
+            if (count)
+                return false;
+        return true;
     }
 };
