25c42565ecfaa9a941f0de64f347d945d8126400
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2023-12-25 21:24:43 +0800

    Update 91. Decode Ways.cpp

--- 91. Decode Ways.cpp
+++ 91. Decode Ways.cpp
@@ -3,13 +3,12 @@ class Solution
 public:
     int numDecodings(string s)
     {
-        int length = s.length(), one = 1, two;
-        for (int i = length - 1; i >= 0; --i)
+        int one = s.back() != '0', two = 1;
+        for (int i = s.length() - 2; i >= 0; --i)
         {
             int cur = s[i] == '0' ? 0 : one;
-            if (i < length - 1)
-                if (s[i] == '1' || s[i] == '2' && s[i + 1] < '7')
-                    cur += two;
+            if (s[i] == '1' || s[i] == '2' && s[i + 1] <= '6')
+                cur += two;
             two = one;
             one = cur;
         }
