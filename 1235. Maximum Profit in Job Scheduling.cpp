aafc145a50596b7e46d70bc80ae999eba8a3a24a
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2024-01-06 10:21:54 +0800

    Update 1235. Maximum Profit in Job Scheduling.cpp

--- 1235. Maximum Profit in Job Scheduling.cpp
+++ 1235. Maximum Profit in Job Scheduling.cpp
@@ -3,16 +3,15 @@ class Solution
 public:
     int jobScheduling(vector<int> &startTime, vector<int> &endTime, vector<int> &profit)
     {
-        size_t size = profit.size();
-        vector<vector<int>> jobs;
-        for (size_t i = 0; i < size; ++i)
+        vector<vector<int>> jobs{};
+        for (int i = startTime.size() - 1; i >= 0; --i)
             jobs.push_back({endTime[i], startTime[i], profit[i]});
-        sort(jobs.begin(), jobs.end()); // sort by end time
+        sort(jobs.begin(), jobs.end());
         map<int, int> dp{{0, 0}};
-        for (vector<int> &job : jobs)
+        for (const vector<int> &job : jobs)
         {
-            int cur = prev(dp.upper_bound(job[1]))->second + job[2]; // find last job whose end time <= current start time
-            if (cur > dp.rbegin()->second)
+            const int cur = prev(dp.upper_bound(job[1]))->second + job[2];
+            if (dp.rbegin()->second < cur)
                 dp[job[0]] = cur;
         }
         return dp.rbegin()->second;
