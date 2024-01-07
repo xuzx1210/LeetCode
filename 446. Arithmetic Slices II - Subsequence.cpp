c87c91017d1f6007b1a4c9d89f9a0980395f8aa6
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2024-01-07 16:15:20 +0800

    Update 446. Arithmetic Slices II - Subsequence.cpp

--- 446. Arithmetic Slices II - Subsequence.cpp
+++ 446. Arithmetic Slices II - Subsequence.cpp
@@ -3,16 +3,15 @@ class Solution
 public:
     int numberOfArithmeticSlices(vector<int> &nums)
     {
-        size_t size = nums.size();
         int result = 0;
-        vector<unordered_map<int64_t, int>> dp(size);
-        for (size_t i = 1; i < size; ++i)
-            for (size_t j = 0; j < i; ++j)
+        const int size = nums.size();
+        vector<unordered_map<long, int>> dp(size, unordered_map<long, int>{});
+        for (int i = 1; i < size; ++i)
+            for (int j = 0; j < i; ++j)
             {
-                int64_t delta = (int64_t)nums[i] - nums[j];
-                int sum = dp[j][delta];
-                dp[i][delta] += sum + 1;
-                result += sum;
+                const long delta = (long)nums[i] - nums[j];
+                result += dp[j][delta];
+                dp[i][delta] += dp[j][delta] + 1;
             }
         return result;
     }
