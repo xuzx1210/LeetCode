b6e0c4541a26197ec2c74301c812cd17359ff273
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2024-01-08 16:23:23 +0800

    Update 938. Range Sum of BST.cpp

--- 938. Range Sum of BST.cpp
+++ 938. Range Sum of BST.cpp
@@ -3,15 +3,14 @@ class Solution
 public:
     int rangeSumBST(TreeNode *root, int low, int high)
     {
-        if (!root)
+        if (root == nullptr)
             return 0;
-        int result = 0;
-        if (low < root->val)
+        const bool left = low < root->val, right = root->val < high;
+        int result = root->val * (low == root->val || left && right || root->val == high);
+        if (left)
             result += rangeSumBST(root->left, low, high);
-        if (root->val < high)
+        if (right)
             result += rangeSumBST(root->right, low, high);
-        if (low <= root->val && root->val <= high)
-            result += root->val;
         return result;
     }
 };
