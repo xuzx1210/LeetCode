68b6b2ba8471bbf94f7aff43c0e7f19c727366c5
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2024-01-10 00:19:24 +0800

    Update 872. Leaf-Similar Trees.cpp

--- 872. Leaf-Similar Trees.cpp
+++ 872. Leaf-Similar Trees.cpp
@@ -1,22 +1,31 @@
 class Solution
 {
 private:
-    void dfs(TreeNode *node, vector<int> &sequence)
+    int getNext(stack<TreeNode *> &tree)
     {
-        if (node == nullptr)
-            return;
-        dfs(node->left, sequence);
-        dfs(node->right, sequence);
-        if (node->left == nullptr && node->right == nullptr)
-            sequence.emplace_back(node->val);
+        int result = -1;
+        while (!tree.empty())
+        {
+            TreeNode *curr = tree.top();
+            tree.pop();
+            if (curr == nullptr)
+                continue;
+            result = curr->val;
+            tree.emplace(curr->left);
+            tree.emplace(curr->right);
+            if (curr->left == nullptr && curr->right == nullptr)
+                break;
+        }
+        return result;
     }
 
 public:
     bool leafSimilar(TreeNode *root1, TreeNode *root2)
     {
-        vector<int> sequence1{}, sequence2{};
-        dfs(root1, sequence1);
-        dfs(root2, sequence2);
-        return sequence1 == sequence2;
+        stack<TreeNode *> tree1({root1}), tree2({root2});
+        while (!tree1.empty() && !tree2.empty())
+            if (getNext(tree1) != getNext(tree2))
+                return false;
+        return tree1.empty() && tree2.empty();
     }
 };
