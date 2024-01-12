9e6f93fbf7867661c7c349d2d952b39a34cad26c
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2024-01-12 09:47:40 +0800

    Update 1704. Determine if String Halves Are Alike.cpp

--- 1704. Determine if String Halves Are Alike.cpp
+++ 1704. Determine if String Halves Are Alike.cpp
@@ -3,12 +3,14 @@ class Solution
 public:
     bool halvesAreAlike(string s)
     {
-        const size_t length = s.length();
-        const size_t half = length >> 1;
-        size_t a = 0, b = 0;
-        for (size_t i = 0; i < length; ++i)
+        const int length = s.length(), half = length >> 1;
+        int a = 0, b = 0;
+        for (int i = 0; i < half; ++i)
             if (0x208222 >> (s[i] & 0x1f) & 1)
-                ++(i < half ? a : b);
+                ++a;
+        for (int i = half; i < length; ++i)
+            if (0x208222 >> (s[i] & 0x1f) & 1)
+                ++b;
         return a == b;
     }
 };
