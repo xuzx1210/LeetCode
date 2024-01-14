bb89050bb6098ca966f5cca12df465c7d92fa8ba
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2024-01-14 17:29:06 +0800

    Update 1657. Determine if Two Strings Are Close.cpp

--- 1657. Determine if Two Strings Are Close.cpp
+++ 1657. Determine if Two Strings Are Close.cpp
@@ -3,16 +3,16 @@ class Solution
 public:
     bool closeStrings(string word1, string word2)
     {
-        vector<int> count1(26), count2(26);
-        for (char c : word1)
-            ++count1[c - 'a'];
-        for (char c : word2)
-            ++count2[c - 'a'];
-        for (size_t i = 0; i < 26; ++i)
-            if (count1[i] && !count2[i] || !count1[i] && count2[i])
+        vector<int> counter1(26, 0), counter2(26, 0);
+        for (const char c : word1)
+            ++counter1[c - 'a'];
+        for (const char c : word2)
+            ++counter2[c - 'a'];
+        for (int i = 0; i < 26; ++i)
+            if (counter1[i] && !counter2[i] || !counter1[i] && counter2[i])
                 return false;
-        sort(count1.begin(), count1.end());
-        sort(count2.begin(), count2.end());
-        return count1 == count2;
+        sort(counter1.begin(), counter1.end());
+        sort(counter2.begin(), counter2.end());
+        return counter1 == counter2;
     }
 };
