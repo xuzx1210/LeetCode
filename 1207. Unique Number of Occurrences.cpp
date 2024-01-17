45548363526568daaf7c373fc1e9ec2848237d40
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2024-01-17 20:18:07 +0800

    Update 1207. Unique Number of Occurrences.cpp

--- 1207. Unique Number of Occurrences.cpp
+++ 1207. Unique Number of Occurrences.cpp
@@ -3,15 +3,15 @@ class Solution
 public:
     bool uniqueOccurrences(vector<int> &arr)
     {
-        unordered_map<int, int> countNumber;
-        unordered_map<int, bool> countOccurrence;
-        for (int &number : arr)
-            ++countNumber[number];
-        for (auto &[key, value] : countNumber)
-            if (countOccurrence[value])
-                return false;
+        unordered_map<int, int> occurrences{};
+        for (const int num : arr)
+            ++occurrences[num];
+        unordered_set<int> countOccurrence{};
+        for (const auto &[num, occurrence] : occurrences)
+            if (countOccurrence.find(occurrence) == countOccurrence.end())
+                countOccurrence.emplace(occurrence);
             else
-                countOccurrence[value] = true;
+                return false;
         return true;
     }
 };
