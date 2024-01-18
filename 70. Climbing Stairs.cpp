c6d9e17a9076528117c8b6159c761769a1e831cd
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2024-01-18 20:26:15 +0800

    Update 70. Climbing Stairs.cpp

--- 70. Climbing Stairs.cpp
+++ 70. Climbing Stairs.cpp
@@ -2,13 +2,12 @@ class Solution
 {
 public:
     int climbStairs(int n)
-    { // initial index is -1, 0, 1
-        int prev2 = 0, prev1 = 1, result = 1;
-        for (int i = 2; i <= n; ++i)
+    {
+        int tmp = 1, result = 1;
+        while (--n)
         {
-            prev2 = prev1;
-            prev1 = result;
-            result = prev2 + prev1;
+            swap(tmp, result);
+            result += tmp;
         }
         return result;
     }
