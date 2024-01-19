08b341dc83026a454e151b2f559983adcea89a2a
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2024-01-19 13:11:05 +0800

    Update 931. Minimum Falling Path Sum.cpp

--- 931. Minimum Falling Path Sum.cpp
+++ 931. Minimum Falling Path Sum.cpp
@@ -4,19 +4,13 @@ public:
     int minFallingPathSum(vector<vector<int>> &matrix)
     {
         const int n = matrix.size();
-        if (n == 1)
-            return matrix[0][0];
-        for (int i = n - 2; i >= 0; --i)
-            for (int j = 0; j < n; ++j)
-                if (j == 0)
-                    matrix[i][j] += min(matrix[i + 1][j], matrix[i + 1][j + 1]);
-                else if (j == n - 1)
-                    matrix[i][j] += min(matrix[i + 1][j - 1], matrix[i + 1][j]);
-                else
-                    matrix[i][j] += min(matrix[i + 1][j], min(matrix[i + 1][j - 1], matrix[i + 1][j + 1]));
-        int result = INT_MAX;
-        for (int j = 0; j < n; ++j)
-            result = min(result, matrix[0][j]);
-        return result;
+        for (int i = 1; i < n; ++i)
+        {
+            matrix[i][0] += min(matrix[i - 1][0], matrix[i - 1][1]);
+            for (int j = 1; j < n - 1; ++j)
+                matrix[i][j] += min({matrix[i - 1][j - 1], matrix[i - 1][j], matrix[i - 1][j + 1]});
+            matrix[i][n - 1] += min(matrix[i - 1][n - 2], matrix[i - 1][n - 1]);
+        }
+        return *min_element(matrix.back().begin(), matrix.back().end());
     }
 };
