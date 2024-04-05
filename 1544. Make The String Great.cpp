7cd77bac89b817c0e245f68f29f4b73c5b7ebb1c
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2024-04-06 01:21:19 +0800

    Update 1544. Make The String Great.cpp

--- 1544. Make The String Great.cpp
+++ 1544. Make The String Great.cpp
@@ -3,23 +3,12 @@ class Solution
 public:
     string makeGood(string s)
     {
-        string result;
-        int length = s.length();
-        for (int i = 0; i < length;)
-        {
-            result.push_back(s[i++]);
-            while (!result.empty() && i < length)
-            {
-                char back = result.back();
-                if (back - 'a' == s[i] - 'A' || back - 'A' == s[i] - 'a')
-                {
-                    result.pop_back();
-                    ++i;
-                }
-                else
-                    break;
-            }
-        }
+        string result{};
+        for (const char c : s)
+            if (!result.empty() && (result.back() - c == 32 || c - result.back() == 32))
+                result.pop_back();
+            else
+                result.push_back(c);
         return result;
     }
 };
