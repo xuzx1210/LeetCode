1487568ef778f9cb2737453639296c3ec47d149f
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2024-05-19 14:27:59 +0800

    Create 3068. Find the Maximum Sum of Node Values.cpp

---
+++ 3068. Find the Maximum Sum of Node Values.cpp
@@ -0,0 +1,48 @@
+// branch
+class Solution
+{
+public:
+    long long maximumValueSum(vector<int> &nums, int k, vector<vector<int>> &edges)
+    {
+        long long result = accumulate(nums.begin(), nums.end(), 0LL);
+        int minAbsDiff = numeric_limits<int>::max();
+        bool odd = false;
+        for (const int num : nums)
+        {
+            const int diff = (num ^ k) - num;
+            if (0 < diff)
+            {
+                result += diff;
+                odd = !odd;
+            }
+            minAbsDiff = min(minAbsDiff, abs(diff));
+        }
+        if (odd)
+            result -= minAbsDiff;
+        return result;
+    }
+};
+
+//branchless
+class Solution
+{
+public:
+    long long maximumValueSum(vector<int> &nums, int k, vector<vector<int>> &edges)
+    {
+        long long result = accumulate(nums.begin(), nums.end(), 0LL);
+        long long minAbsDiff = numeric_limits<long long>::max();
+        bool odd = false;
+        for (const int num : nums)
+        {
+            long long diff = (num ^ k) - num;
+            const bool positive = (0 < diff);
+            result += positive * diff;
+            odd = odd != positive;
+            diff = positive * diff - !positive * diff;
+            const bool less = (diff < minAbsDiff);
+            minAbsDiff = less * diff + !less * minAbsDiff;
+        }
+        result -= odd * minAbsDiff;
+        return result;
+    }
+};
