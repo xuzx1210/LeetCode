a48d1a908ce6e0151c77496c205b75e432fab765
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2024-05-26 08:33:57 +0800

    Update 140. Word Break II.cpp

--- 140. Word Break II.cpp
+++ 140. Word Break II.cpp
@@ -1,54 +1,92 @@
 class Solution
 {
-public:
-    string s;
-    int length;
-    vector<string> wordDict;
-    vector<string> cur;
-    set<vector<string>> all;
-    bool equal(int index, string &word)
+private:
+    vector<string> result;
+    vector<string> path;
+    class TrieNode
     {
-        if (index + word.length() > s.length())
-            return false;
-        for (int i = 0; i < word.length(); ++i)
-            if (s[index + i] != word[i])
-                return false;
-        return true;
-    }
-    void dfs(int index)
-    {
-        if (index > length)
-            return;
-        if (index == length)
+    private:
+        bool stop;
+        vector<TrieNode *> next;
+
+    public:
+        TrieNode()
         {
-            all.insert(cur);
+            next.resize(26, nullptr);
+            stop = false;
+        }
+        void insert(const int index, const string &word)
+        {
+            if (index == word.length())
+            {
+                this->stop = true;
+                return;
+            }
+            const int i = word[index] - 'a';
+            if (!next[i])
+                next[i] = new TrieNode();
+            next[i]->insert(index + 1, word);
+        }
+        bool isWord()
+        {
+            return stop;
+        }
+        bool hasNext(const char c)
+        {
+            return next[c - 'a'];
+        }
+        TrieNode *go(const char c)
+        {
+            return next[c - 'a'];
+        }
+    };
+    class Trie
+    {
+    public:
+        TrieNode *root;
+        Trie()
+        {
+            root = new TrieNode();
+        }
+        void insert(const string &word)
+        {
+            root->insert(0, word);
+        }
+    };
+    void dfs(const int index, const string &s, Trie &trie)
+    {
+        if (index == s.length())
+        {
+            string tmp(path.front());
+            for (int i = 1; i < path.size(); ++i)
+                tmp.append(" " + path[i]);
+            result.emplace_back(tmp);
             return;
         }
-        for (string &word : wordDict)
-            if (equal(index, word))
-            {
-                cur.push_back(word);
-                dfs(index + word.length());
-                cur.pop_back();
-            }
+        TrieNode *node = trie.root;
+        for (int i = index; i < s.length(); ++i)
+        {
+            const int c = s[i] - 'a';
+            if (!node->hasNext(s[i]))
+                break;
+            node = node->go(s[i]);
+            if (!node->isWord())
+                continue;
+            path.emplace_back(s, index, i - index + 1);
+            dfs(i + 1, s, trie);
+            path.pop_back();
+        }
     }
+
+public:
     vector<string> wordBreak(string s, vector<string> &wordDict)
     {
-        this->s = s;
-        length = s.length();
-        this->wordDict = wordDict;
-        cur.clear();
-        all.clear();
-        dfs(0);
-        vector<string> result({});
-        for (auto &one : all)
-        {
-            string tmp("");
-            for (auto &word : one)
-                tmp += (word + " ");
-            tmp.pop_back();
-            result.push_back(tmp);
-        }
+        result.clear();
+        path.clear();
+        Trie trie;
+        for (const string &word : wordDict)
+            trie.insert(word);
+        dfs(0, s, trie);
         return result;
     }
 };
