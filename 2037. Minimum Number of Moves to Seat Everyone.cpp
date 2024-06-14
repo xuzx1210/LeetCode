a21b3bfc75f9f07db835e52014330441d17f501a
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2024-06-14 20:32:36 +0800

    Create 2037. Minimum Number of Moves to Seat Everyone.cpp

---
+++ 2037. Minimum Number of Moves to Seat Everyone.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+    int minMovesToSeat(vector<int> &seats, vector<int> &students)
+    {
+        int result = 0;
+        const int n = seats.size();
+        sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
+        for (int i = 0; i < n; ++i)
+            result += abs(seats[i] - students[i]);
+        return result;
+    }
+};
