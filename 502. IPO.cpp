ce1768da62a1eb0567e9f533897f35af87d96650
xuzx1210 <83566263+xuzx1210@users.noreply.github.com>
2024-06-16 13:23:43 +0800

    Update 502. IPO.cpp

--- 502. IPO.cpp
+++ 502. IPO.cpp
@@ -1,27 +1,24 @@
 class Solution
 {
+
 public:
     int findMaximizedCapital(int k, int w, vector<int> &profits, vector<int> &capital)
     {
         const int n = profits.size();
-        priority_queue<int> maxHeap{};
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap{};
+        vector<pair<int, int>> projects{};
         for (int i = 0; i < n; ++i)
-            minHeap.emplace(capital[i], profits[i]);
-        for (int i = 0; i < k; ++i)
+            projects.emplace_back(capital[i], i);
+        sort(projects.begin(), projects.end());
+        priority_queue<int> toIPO{};
+        int availableProjects = 0;
+        while (k--)
         {
-            while (!minHeap.empty())
-                if (minHeap.top().first <= w)
-                {
-                    maxHeap.emplace(minHeap.top().second);
-                    minHeap.pop();
-                }
-                else
-                    break;
-            if (maxHeap.empty())
+            for (; availableProjects < n && projects[availableProjects].first <= w; ++availableProjects)
+                toIPO.emplace(profits[projects[availableProjects].second]);
+            if (toIPO.empty())
                 break;
-            w += maxHeap.top();
-            maxHeap.pop();
+            w += toIPO.top();
+            toIPO.pop();
         }
         return w;
     }
